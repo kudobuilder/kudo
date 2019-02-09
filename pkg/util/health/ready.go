af0435335cce74ee6956ee3c4475e079fb360d65
Thomas Runyon <runyontr@gmail.com>
2019-02-09 14:51:46 -0500

    Initial commit to showcase a Flink example.  (#82)
    * WIP initial commit to showcase a Flint example.  Allows for health check of Instance objects to be used in steps of Plan
    
    * WIP added flink and generator/actos to demo file
    
    * WIP
    
    * Updates for link working
    
    * Added scratch section for manually triggering upload of jar
    
    * updated to latest mesosphere/flink-demo-actor and created job that submits flink-job
    
    * Actually fix the merge issues

--- pkg/util/health/ready.go
+++ pkg/util/health/ready.go
@@ -2,7 +2,9 @@ package health
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+
 	maestrov1alpha1 "github.com/maestrosdk/maestro/pkg/apis/maestro/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
@@ -17,8 +19,16 @@ func IsHealthy(c client.Client, obj runtime.Object) error {
 	switch obj.(type) {
 	case *appsv1.StatefulSet:
 		ss := obj.(*appsv1.StatefulSet)
-		if ss.Status.ReadyReplicas == ss.Status.Replicas {
-			log.Printf("HealthUtil: Statefulset %v is marked healthy", ss.Name)
+		log.Println("------HEALTH---------")
+		log.Printf("Looking at Statefulset: %v\n", ss.Name)
+		b, _ := json.MarshalIndent(ss, "", "\t")
+		log.Printf("\n%v\n", string(b))
+		log.Println("---------------------")
+		if ss.Spec.Replicas == nil {
+			return fmt.Errorf("replicas not set, so can't be healthy")
+		}
+		if ss.Status.ReadyReplicas == *ss.Spec.Replicas {
+			log.Printf("Statefulset %v is marked healthy\n", ss.Name)
 			return nil
 		}
 		log.Printf("HealthUtil: Statefulset %v is NOT healthy. Not enough ready replicas: %v/%v", ss.Name, ss.Status.ReadyReplicas, ss.Status.Replicas)
@@ -46,11 +56,16 @@ func IsHealthy(c client.Client, obj runtime.Object) error {
 		i := obj.(*maestrov1alpha1.Instance)
 		//Instances are healthy when their Active Plan has succeeded
 		plan := &maestrov1alpha1.PlanExecution{}
-		c.Get(context.TODO(), client.ObjectKey{
+		err := c.Get(context.TODO(), client.ObjectKey{
 			Name:      i.Status.ActivePlan.Name,
 			Namespace: i.Status.ActivePlan.Namespace,
 		}, plan)
+		if err != nil {
+			log.Printf("Error getting PlaneExecution %v/%v: %v\n", i.Status.ActivePlan.Name, i.Status.ActivePlan.Namespace, err)
+			return fmt.Errorf("instance active plan not found: %v", err)
+		}
 		log.Printf("HealthUtil: Instance %v is in state %v", i.Name, plan.Status.State)
+
 		if plan.Status.State == maestrov1alpha1.PhaseStateComplete {
 			return nil
 		}
