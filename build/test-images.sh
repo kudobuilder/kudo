7c05f286dd8d6b6a1ce86dd0f9a074cd6f1f999f
Gerred Dillon <hello@gerred.org>
2019-04-18 01:08:02 -0400

    update build images shell script

--- build/test-images.sh
+++ build/test-images.sh
@@ -7,5 +7,8 @@ docker push kudobuilder/golang:1.10
 docker build --build-arg GOLANG_VERSION=1.11 -t kudobuilder/golang:1.11 .
 docker push kudobuilder/golang:1.11
 
+docker build --build-arg GOLANG_VERSION=1.12 -t kudobuilder/golang:1.12 .
+docker push kudobuilder/golang:1.12
+
 docker build --build-arg GOLANG_VERSION=latest -t kudobuilder/golang:latest .
 docker push kudobuilder/golang:latest
