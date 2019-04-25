3be859d7dd64fac0223ca0d88b91134103565f24
kensipe@gmail.com <ken@mesosphere.io>
2019-04-25 14:42:59 -0500

    caching tools for code gens

--- hack/update_manifests.sh
+++ hack/update_manifests.sh
@@ -4,21 +4,20 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-TMP_DIR=$(mktemp -d)
-
-cleanup() {
-  rm -rf "${TMP_DIR}"
-}
-trap "cleanup" EXIT SIGINT
-
-cleanup
-
 # The following solution for making code generation work with go modules is
 # borrowed and modified from https://github.com/heptio/contour/pull/1010.
+# it has been modified to enable caching.
 export GO111MODULE=on
 VERSION=$(go list -m all | grep sigs.k8s.io/controller-tools | rev | cut -d"-" -f1 | cut -d" " -f1 | rev)
-git clone https://github.com/kubernetes-sigs/controller-tools.git "${TMP_DIR}"
-(cd "${TMP_DIR}" && git reset --hard "${VERSION}" && go mod init)
+CONTROLLER_GEN_DIR="hack/controller-gen/$VERSION"
 
-go run "$TMP_DIR"/cmd/controller-gen/main.go rbac --output-dir=config/default/rbac
-go run "$TMP_DIR"/cmd/controller-gen/main.go crd
+if [ -d $CONTROLLER_GEN_DIR ]   # for file "if [-f /home/rama/file]"
+then
+    echo "Using cached controller generator version: $VERSION"
+else
+  git clone https://github.com/kubernetes-sigs/controller-tools.git "${CONTROLLER_GEN_DIR}"
+  (cd "${CONTROLLER_GEN_DIR}" && git reset --hard "${VERSION}" && go mod init)
+fi
+
+go run "$CONTROLLER_GEN_DIR"/cmd/controller-gen/main.go rbac --output-dir=config/default/rbac
+go run "$CONTROLLER_GEN_DIR"/cmd/controller-gen/main.go crd
