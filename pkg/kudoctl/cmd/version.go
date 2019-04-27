a44bd1dadddc5dca084d20eb71f5b02b8f61d05c
Ken Sipe <ken@mesosphere.io>
2019-04-27 07:21:33 -0500

    make check-formatting failures fixed (#204)

--- pkg/kudoctl/cmd/version.go
+++ pkg/kudoctl/cmd/version.go
@@ -14,7 +14,7 @@ var (
 		kubectl kudo version`
 )
 
-// NewVersionCMD returns a new initialized instance of the version sub command
+// NewVersionCmd returns a new initialized instance of the version sub command
 func NewVersionCmd() *cobra.Command {
 	versionCmd := &cobra.Command{
 		Use:          "version",
@@ -33,7 +33,7 @@ func NewVersionCmd() *cobra.Command {
 	return versionCmd
 }
 
-// VersionCMD performs the version sub command
+// VersionCmd performs the version sub command
 func VersionCmd(cmd *cobra.Command, args []string) error {
 	kudoVersion := version.Get()
 	fmt.Printf("KUDO Version: %s\n", fmt.Sprintf("%#v", kudoVersion))
