7bd9758e49ccdc4f434669abdca22a00c4a882c2
Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>
2019-06-05 15:40:51 +0200

    Use Helm-style parameters for `kudoctl kudo install` command (#283)
    * Use helm style parameters for `kudoctl kudo install` command. So instead of:
    `kubectl kudo install kafka -p foo,bar` it would be: `kubectl kudo install kafka -p foo=bar`
    
    Additionally, parameter validation is now done before the actual command execution to avoid potential failures down the road.
    
    Issue: #225
    
    * Fixing formatting issues
    
    * Review feedback
    
    - used table-driven tests for `TestTableNewInstallCmd_WithParameters`
    - `validateInstallParameters` now returns all invalid parameters and not just the first one

--- pkg/kudoctl/cmd/install_test.go
+++ pkg/kudoctl/cmd/install_test.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewCmdInstallReturnsCmd(t *testing.T) {
@@ -21,5 +23,26 @@ func TestNewCmdInstallReturnsCmd(t *testing.T) {
 	// In case of failure of this test check this PR: spf13/cobra#110
 	if reflect.ValueOf(sub.Flags().GetNormalizeFunc()).Pointer() != reflect.ValueOf(newCmdInstall.Flags().GetNormalizeFunc()).Pointer() {
 		t.Fatal("child and root commands should have the same normalization functions")
+	}
+}
+
+var parameterTests = []struct {
+	flags        []string
+	errorMessage string
+}{
+	{[]string{"foo"}, "a parameter without value worked"},                           // 1
+	{[]string{"bar="}, "a parameter with empty value worked"},                       // 2
+	{[]string{"foo=bar", "fiz="}, "one of many parameters with empty value worked"}, // 3
+	{[]string{"foo", "bar"}, "multiple empty parameters worked"},                    // 4
+}
+
+func TestTableNewInstallCmd_WithParameters(t *testing.T) {
+	for _, test := range parameterTests {
+		newCmdInstall := NewInstallCmd()
+		for _, flag := range test.flags {
+			newCmdInstall.Flags().Set("parameter", flag)
+		}
+		err := newCmdInstall.RunE(newCmdInstall, []string{})
+		assert.NotNil(t, err, test.errorMessage)
 	}
 }
