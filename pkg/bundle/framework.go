6b9555d6245d67739fc9912fa2149a6a21db0e1e
Gerred Dillon <hello@gerred.org>
2019-06-07 08:23:39 -0400

    formatting

--- pkg/bundle/framework.go
+++ pkg/bundle/framework.go
@@ -2,15 +2,15 @@ package bundle
 
 // Framework is a representation of the KEP-9 Framework YAML
 type Framework struct {
-	Name string `json:"name"`
-	Description string `json:"description,omitempty"`
-	Version string `json:"version"`
-	KUDOVersion string `json:"kudoVersion,omitempty"`
-	KubernetesVersion string `json:"kubernetesVersion,omitempty"`
-	Maintainers []string `json:"maintainers,omitempty"`
-	URL string `json:"url,omitempty"`
-	Tasks map[string]Task `json:"tasks"`
-	Plans map[string]Plan `json:"plans"`
+	Name              string          `json:"name"`
+	Description       string          `json:"description,omitempty"`
+	Version           string          `json:"version"`
+	KUDOVersion       string          `json:"kudoVersion,omitempty"`
+	KubernetesVersion string          `json:"kubernetesVersion,omitempty"`
+	Maintainers       []string        `json:"maintainers,omitempty"`
+	URL               string          `json:"url,omitempty"`
+	Tasks             map[string]Task `json:"tasks"`
+	Plans             map[string]Plan `json:"plans"`
 }
 
 // Task is a representation of the KEP-9 Task inside of Framework
@@ -20,20 +20,20 @@ type Task struct {
 
 // Plan is a representation of the KEP-9 Plan inside of Framework
 type Plan struct {
-	Strategy string `json:"string,omitempty"`
-	Phases []Phase `json:"phases'"`
+	Strategy string  `json:"string,omitempty"`
+	Phases   []Phase `json:"phases'"`
 }
 
 // Phase is a representation of the KEP-9 Phase inside of Framework
 type Phase struct {
-	Name string `json:"name"`
+	Name     string `json:"name"`
 	Strategy string `json:"string,omitempty"`
-	Steps []Step `json:"steps"`
+	Steps    []Step `json:"steps"`
 }
 
 // Step is a representation of the KEP-9 Step inside of Framework
 type Step struct {
-	Name string `json:"name"`
-	Tasks []string `json:"tasks"`
-	Delete bool `json:"delete,omitempty"`
+	Name   string   `json:"name"`
+	Tasks  []string `json:"tasks"`
+	Delete bool     `json:"delete,omitempty"`
 }
