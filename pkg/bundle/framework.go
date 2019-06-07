35b3c06f0b7392bcc171c34a5b369856b8d52f99
Gerred Dillon <hello@gerred.org>
2019-06-07 09:01:35 -0400

    Update framework

--- pkg/bundle/framework.go
+++ pkg/bundle/framework.go
@@ -1,39 +1,18 @@
 package bundle
 
+import "github.com/kudobuilder/kudo/pkg/apis/kudo/v1alpha1"
+
 // Framework is a representation of the KEP-9 Framework YAML
 type Framework struct {
-	Name              string          `json:"name"`
-	Description       string          `json:"description,omitempty"`
-	Version           string          `json:"version"`
-	KUDOVersion       string          `json:"kudoVersion,omitempty"`
-	KubernetesVersion string          `json:"kubernetesVersion,omitempty"`
-	Maintainers       []string        `json:"maintainers,omitempty"`
-	URL               string          `json:"url,omitempty"`
-	Tasks             map[string]Task `json:"tasks"`
-	Plans             map[string]Plan `json:"plans"`
-}
-
-// Task is a representation of the KEP-9 Task inside of Framework
-type Task struct {
-	Resources []string `json:"resources"`
-}
-
-// Plan is a representation of the KEP-9 Plan inside of Framework
-type Plan struct {
-	Strategy string  `json:"string,omitempty"`
-	Phases   []Phase `json:"phases'"`
-}
-
-// Phase is a representation of the KEP-9 Phase inside of Framework
-type Phase struct {
-	Name     string `json:"name"`
-	Strategy string `json:"string,omitempty"`
-	Steps    []Step `json:"steps"`
-}
-
-// Step is a representation of the KEP-9 Step inside of Framework
-type Step struct {
-	Name   string   `json:"name"`
-	Tasks  []string `json:"tasks"`
-	Delete bool     `json:"delete,omitempty"`
+	Name              string                         `json:"name"`
+	Description       string                         `json:"description,omitempty"`
+	Version           string                         `json:"version"`
+	KUDOVersion       string                         `json:"kudoVersion,omitempty"`
+	KubernetesVersion string                         `json:"kubernetesVersion,omitempty"`
+	Maintainers       []v1alpha1.Maintainer          `json:"maintainers,omitempty"`
+	URL               string                         `json:"url,omitempty"`
+	Tasks             map[string]v1alpha1.TaskSpec   `json:"tasks"`
+	Plans             map[string]v1alpha1.Plan       `json:"plans"`
+	Parameters        []v1alpha1.Parameter           `json:"parameters,omitempty"`
+	Dependencies      []v1alpha1.FrameworkDependency `json:"dependencies,omitempty"`
 }
