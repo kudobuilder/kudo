889b40242094990936a0d304c746469fafd4e3d8
Alena Varkockova <varkockova.a@gmail.com>
2019-06-11 12:10:00 +0200

    Fixes

--- pkg/kudoctl/cmd/plan/plan_history.go
+++ pkg/kudoctl/cmd/plan/plan_history.go
@@ -3,9 +3,10 @@ package plan
 import (
 	"encoding/json"
 	"fmt"
+	"time"
+
 	"github.com/kudobuilder/kudo/pkg/kudoctl/util/check"
 	"github.com/pkg/errors"
-	"time"
 
 	kudov1alpha1 "github.com/kudobuilder/kudo/pkg/apis/kudo/v1alpha1"
 	"github.com/spf13/cobra"
@@ -16,8 +17,17 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+type historyOptions struct {
+	instance       string
+	kubeConfigPath string
+	namespace      string
+}
+
+var defaultHistoryOptions = &historyOptions{}
+
 // NewPlanHistoryCmd creates a command that shows the plan instory for an instance
 func NewPlanHistoryCmd() *cobra.Command {
+	options := defaultHistoryOptions
 	listCmd := &cobra.Command{
 		//Args: cobra.ExactArgs(1),
 		Use:   "history",
@@ -25,29 +35,31 @@ func NewPlanHistoryCmd() *cobra.Command {
 		Long: `
 	# View plan status
 	kudoctl plan history <frameworkVersion> --instance=<instanceName>`,
-		RunE: runHistory,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runHistory(cmd, args, options)
+		},
 	}
 
-	listCmd.Flags().StringVar(&instance, "instance", "", "The instance name.")
-	listCmd.Flags().StringVar(&kubeConfig, "kubeconfig", "", "The file path to kubernetes configuration file; defaults to $HOME/.kube/config")
-	listCmd.Flags().StringVar(&namespace, "namespace", "default", "The namespace where the operator watches for changes.")
+	listCmd.Flags().StringVar(&options.instance, "instance", "", "The instance name.")
+	listCmd.Flags().StringVar(&options.kubeConfigPath, "kubeconfig", "", "The file path to kubernetes configuration file; defaults to $HOME/.kube/config")
+	listCmd.Flags().StringVar(&options.namespace, "namespace", "default", "The namespace where the operator watches for changes.")
 
 	return listCmd
 }
 
-func runHistory(cmd *cobra.Command, args []string) error {
+func runHistory(cmd *cobra.Command, args []string, options *historyOptions) error {
 
 	instanceFlag, err := cmd.Flags().GetString("instance")
 	if err != nil || instanceFlag == "" {
-		return fmt.Errorf("Flag Error: Please set instance flag, e.g. \"--instance=<instanceName>\"")
+		return fmt.Errorf("flag Error: Please set instance flag, e.g. \"--instance=<instanceName>\"")
 	}
 
-	configPath, err := check.KubeConfigLocationOrDefault(kubeConfig)
+	configPath, err := check.KubeConfigLocationOrDefault(options.kubeConfigPath)
 	if err != nil {
 		return fmt.Errorf("error when getting default kubeconfig path: %+v", err)
 	}
-	kubeConfig = configPath
-	if err := check.ValidateKubeConfigPath(kubeConfig); err != nil {
+	options.kubeConfigPath = configPath
+	if err := check.ValidateKubeConfigPath(options.kubeConfigPath); err != nil {
 		return errors.WithMessage(err, "could not check kubeconfig path")
 	}
 
@@ -57,16 +69,16 @@ func runHistory(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("flag Error: %v", err)
 	}
 
-	err = planHistory(args)
+	err = planHistory(args, options)
 	if err != nil {
 		return fmt.Errorf("client Error: %v", err)
 	}
 	return nil
 }
 
-func planHistory(args []string) error {
+func planHistory(args []string, options *historyOptions) error {
 
-	config, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
+	config, err := clientcmd.BuildConfigFromFlags("", options.kubeConfigPath)
 	if err != nil {
 		return err
 	}
@@ -85,14 +97,14 @@ func planHistory(args []string) error {
 
 	var labelSelector string
 	if len(args) == 0 {
-		fmt.Printf("History of all plan-executions for instance \"%s\" in namespace \"%s\":\n", instance, namespace)
-		labelSelector = "instance=" + instance
+		fmt.Printf("History of all plan-executions for instance \"%s\" in namespace \"%s\":\n", options.instance, options.namespace)
+		labelSelector = "instance=" + options.instance
 	} else {
-		fmt.Printf("History of plan-executions for instance \"%s\" in namespace \"%s\" to framework-version \"%s\":\n", instance, namespace, args[0])
-		labelSelector = "framework-version=" + args[0] + ", instance=" + instance
+		fmt.Printf("History of plan-executions for instance \"%s\" in namespace \"%s\" to framework-version \"%s\":\n", options.instance, options.namespace, args[0])
+		labelSelector = "framework-version=" + args[0] + ", instance=" + options.instance
 	}
 
-	instObj, err := dynamicClient.Resource(planExecutionsGVR).Namespace(namespace).List(metav1.ListOptions{
+	instObj, err := dynamicClient.Resource(planExecutionsGVR).Namespace(options.namespace).List(metav1.ListOptions{
 		LabelSelector: labelSelector,
 	})
 	if err != nil {
@@ -114,7 +126,7 @@ func planHistory(args []string) error {
 	tree := treeprint.New()
 
 	if len(planExecutionList.Items) == 0 {
-		fmt.Printf("No history found for \"%s\" in namespace \"%s\".\n", instance, namespace)
+		fmt.Printf("No history found for \"%s\" in namespace \"%s\".\n", options.instance, options.namespace)
 	} else {
 		for _, i := range planExecutionList.Items {
 			duration := time.Since(i.CreationTimestamp.Time)
--- pkg/kudoctl/cmd/plan/plan_status.go
+++ pkg/kudoctl/cmd/plan/plan_status.go
@@ -3,6 +3,7 @@ package plan
 import (
 	"encoding/json"
 	"fmt"
+
 	kudov1alpha1 "github.com/kudobuilder/kudo/pkg/apis/kudo/v1alpha1"
 	"github.com/kudobuilder/kudo/pkg/kudoctl/util/check"
 	"github.com/pkg/errors"
@@ -14,37 +15,48 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+type statusOptions struct {
+	instance       string
+	kubeConfigPath string
+	namespace      string
+}
+
+var defaultStatusOptions = &statusOptions{}
+
 //NewPlanStatusCmd creates a new command that shows the status of an instance by looking at its current plan
 func NewPlanStatusCmd() *cobra.Command {
+	options := defaultStatusOptions
 	statusCmd := &cobra.Command{
 		Use:   "status",
 		Short: "Shows the status of all plans to an particular instance.",
 		Long: `
 	# View plan status
 	kudoctl plan status --instance=<instanceName> --kubeconfig=<$HOME/.kube/config>`,
-		RunE: runStatus,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runStatus(cmd, args, options)
+		},
 	}
 
-	statusCmd.Flags().StringVar(&instance, "instance", "", "The instance name available from 'kubectl get instances'")
-	statusCmd.Flags().StringVar(&kubeConfig, "kubeconfig", "", "The file path to kubernetes configuration file; defaults to $HOME/.kube/config")
-	statusCmd.Flags().StringVar(&namespace, "namespace", "default", "The namespace where the instance is running.")
+	statusCmd.Flags().StringVar(&options.instance, "instance", "", "The instance name available from 'kubectl get instances'")
+	statusCmd.Flags().StringVar(&options.kubeConfigPath, "kubeconfig", "", "The file path to kubernetes configuration file; defaults to $HOME/.kube/config")
+	statusCmd.Flags().StringVar(&options.namespace, "namespace", "default", "The namespace where the instance is running.")
 
 	return statusCmd
 }
 
-func runStatus(cmd *cobra.Command, args []string) error {
+func runStatus(cmd *cobra.Command, args []string, options *statusOptions) error {
 
 	instanceFlag, err := cmd.Flags().GetString("instance")
 	if err != nil || instanceFlag == "" {
 		return fmt.Errorf("flag Error: Please set instance flag, e.g. \"--instance=<instanceName>\"")
 	}
 
-	configPath, err := check.KubeConfigLocationOrDefault(kubeConfig)
+	configPath, err := check.KubeConfigLocationOrDefault(options.kubeConfigPath)
 	if err != nil {
 		return fmt.Errorf("error when getting default kubeconfig path: %+v", err)
 	}
-	kubeConfig = configPath
-	if err := check.ValidateKubeConfigPath(kubeConfig); err != nil {
+	options.kubeConfigPath = configPath
+	if err := check.ValidateKubeConfigPath(options.kubeConfigPath); err != nil {
 		return errors.WithMessage(err, "could not check kubeconfig path")
 	}
 
@@ -53,18 +65,18 @@ func runStatus(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("flag Error: Please set kubeconfig flag, e.g. \"--kubeconfig=<$HOME/.kube/config>\"")
 	}
 
-	err = planStatus()
+	err = planStatus(options)
 	if err != nil {
 		return fmt.Errorf("client Error: %v", err)
 	}
 	return nil
 }
 
-func planStatus() error {
+func planStatus(options *statusOptions) error {
 
 	tree := treeprint.New()
 
-	config, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
+	config, err := clientcmd.BuildConfigFromFlags("", options.kubeConfigPath)
 	if err != nil {
 		return err
 	}
@@ -81,7 +93,7 @@ func planStatus() error {
 		Resource: "instances",
 	}
 
-	instObj, err := dynamicClient.Resource(instancesGVR).Namespace(namespace).Get(instance, metav1.GetOptions{})
+	instObj, err := dynamicClient.Resource(instancesGVR).Namespace(options.namespace).Get(options.instance, metav1.GetOptions{})
 	if err != nil {
 		return err
 	}
@@ -107,7 +119,7 @@ func planStatus() error {
 	}
 
 	//  List all of the Virtual Services.
-	frameworkObj, err := dynamicClient.Resource(frameworkGVR).Namespace(namespace).Get(frameworkVersionNameOfInstance, metav1.GetOptions{})
+	frameworkObj, err := dynamicClient.Resource(frameworkGVR).Namespace(options.namespace).Get(frameworkVersionNameOfInstance, metav1.GetOptions{})
 	if err != nil {
 		return err
 	}
@@ -130,7 +142,7 @@ func planStatus() error {
 		Resource: "planexecutions",
 	}
 
-	activePlanObj, err := dynamicClient.Resource(planExecutionsGVR).Namespace(namespace).Get(instance.Status.ActivePlan.Name, metav1.GetOptions{})
+	activePlanObj, err := dynamicClient.Resource(planExecutionsGVR).Namespace(options.namespace).Get(instance.Status.ActivePlan.Name, metav1.GetOptions{})
 	if err != nil {
 		return err
 	}
@@ -180,7 +192,7 @@ func planStatus() error {
 		}
 	}
 
-	fmt.Printf("Plan(s) for \"%s\" in namespace \"%s\":\n", instance.Name, namespace)
+	fmt.Printf("Plan(s) for \"%s\" in namespace \"%s\":\n", instance.Name, options.namespace)
 	fmt.Println(tree.String())
 
 	return nil
