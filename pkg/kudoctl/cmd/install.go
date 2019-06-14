0a349b8c90e8486543ea5f238e8769adfaf538eb
Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>
2019-06-14 18:21:46 +0200

    Refactored install parameters (#339)
    * Refactored install parameters
    
    Summary:
    - parameter parsing and validation now happens prior to install command execution
    - `Options.Parameters map[string]string` is now a map with keys and values
    
    * `parseParameters` now returns a parameter map instead of filling one

--- pkg/kudoctl/cmd/install.go
+++ pkg/kudoctl/cmd/install.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/kudobuilder/kudo/pkg/kudoctl/cmd/install"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
@@ -19,15 +21,53 @@ var (
 		kubectl kudo install kafka --package-version=0`
 )
 
+// parseParameters parse raw parameter strings into a map of keys and values
+func parseParameters(raw []string) (map[string]string, error) {
+	var errs []string
+	parameters := make(map[string]string)
+
+	for _, a := range raw {
+		// Using '=' as the delimiter. Split after the first delimiter to support using '=' in values
+		s := strings.SplitN(a, "=", 2)
+		if len(s) < 2 {
+			errs = append(errs, fmt.Sprintf("parameter not set: %+v", a))
+			continue
+		}
+		if s[0] == "" {
+			errs = append(errs, fmt.Sprintf("parameter name can not be empty: %+v", a))
+			continue
+		}
+		if s[1] == "" {
+			errs = append(errs, fmt.Sprintf("parameter value can not be empty: %+v", a))
+			continue
+		}
+		parameters[s[0]] = s[1]
+	}
+
+	if errs != nil {
+		return nil, errors.New(strings.Join(errs, ", "))
+	}
+
+	return parameters, nil
+}
+
 // newInstallCmd creates the install command for the CLI
 func newInstallCmd() *cobra.Command {
 	options := install.DefaultOptions
+	var parameters []string
 	installCmd := &cobra.Command{
 		Use:     "install <name>",
 		Short:   "-> Install an official KUDO package.",
 		Long:    `Install a KUDO package from the official GitHub repo.`,
 		Example: installExample,
 		RunE: func(cmd *cobra.Command, args []string) error {
+			// Prior to command execution we parse and validate passed parameters
+			var err error
+			options.Parameters, err = parseParameters(parameters)
+			if err != nil {
+				return errors.WithMessage(err, "could not parse parameters")
+			}
+
 			return install.Run(cmd, args, options)
 		},
 		SilenceUsage: true,
@@ -38,7 +78,7 @@ func newInstallCmd() *cobra.Command {
 	installCmd.Flags().StringVar(&options.KubeConfigPath, "kubeconfig", "", "The file path to Kubernetes configuration file. (default \"$HOME/.kube/config\")")
 	installCmd.Flags().StringVar(&options.InstanceName, "instance", "", "The instance name. (default to Framework name)")
 	installCmd.Flags().StringVar(&options.Namespace, "namespace", "default", "The namespace used for the package installation. (default \"default\"")
-	installCmd.Flags().StringArrayVarP(&options.Parameters, "parameter", "p", nil, "The parameter name and value separated by '='")
+	installCmd.Flags().StringArrayVarP(&parameters, "parameter", "p", nil, "The parameter name and value separated by '='")
 	installCmd.Flags().StringVar(&options.PackageVersion, "package-version", "", "A specific package version on the official GitHub repo. (default to the most recent)")
 
 	const usageFmt = "Usage:\n  %s\n\nFlags:\n%s"
