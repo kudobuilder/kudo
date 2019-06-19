14551b70ad7a3b330364ba750098ec8d4007fac9
kensipe@gmail.com <ken@mesosphere.io>
2019-06-19 09:58:26 -0500

    removing the bulk of generated tests that are used by kubebuilder

--- pkg/apis/kudo/v1alpha1/frameworkversion_types_test.go
+++ pkg/apis/kudo/v1alpha1/frameworkversion_types_test.go
@@ -21,45 +21,8 @@ import (
 
 	"gopkg.in/go-playground/validator.v9"
 	"k8s.io/apimachinery/pkg/runtime"
-
-	"github.com/onsi/gomega"
-	"golang.org/x/net/context"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 )
 
-func TestStorageFrameworkVersion(t *testing.T) {
-	key := types.NamespacedName{
-		Name:      "foo",
-		Namespace: "default",
-	}
-	created := &FrameworkVersion{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "foo",
-			Namespace: "default",
-		},
-	}
-	g := gomega.NewGomegaWithT(t)
-
-	// Test Create
-	fetched := &FrameworkVersion{}
-	g.Expect(c.Create(context.TODO(), created)).NotTo(gomega.HaveOccurred())
-
-	g.Expect(c.Get(context.TODO(), key, fetched)).NotTo(gomega.HaveOccurred())
-	g.Expect(fetched).To(gomega.Equal(created))
-
-	// Test Updating the Labels
-	updated := fetched.DeepCopy()
-	updated.Labels = map[string]string{"hello": "world"}
-	g.Expect(c.Update(context.TODO(), updated)).NotTo(gomega.HaveOccurred())
-
-	g.Expect(c.Get(context.TODO(), key, fetched)).NotTo(gomega.HaveOccurred())
-	g.Expect(fetched).To(gomega.Equal(updated))
-
-	// Test Delete
-	g.Expect(c.Delete(context.TODO(), fetched)).NotTo(gomega.HaveOccurred())
-	g.Expect(c.Get(context.TODO(), key, fetched)).To(gomega.HaveOccurred())
-}
 
 func TestFrameworkType_Validation(t *testing.T) {
 
