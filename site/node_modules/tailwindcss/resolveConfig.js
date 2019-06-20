d20a3e1ad37b865fd7afb712e186b0ca84471fca
Gerred Dillon <hello@gerred.org>
2019-06-20 11:00:12 -0400

    remove node modules

--- site/node_modules/tailwindcss/resolveConfig.js
+++
@@ -1,6 +0,0 @@
-const resolveConfigObjects = require('./lib/util/resolveConfig').default
-const defaultConfig = require('./stubs/defaultConfig.stub.js')
-
-module.exports = function resolveConfig(config) {
-  return resolveConfigObjects([config, defaultConfig])
-}
