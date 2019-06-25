b36b966d3d8fc4e441f8419f1cb17c141d5541fb
Thomas Runyon <runyontr@gmail.com>
2019-06-25 12:38:03 -0400

    reverted changes to test harness

--- pkg/test/harness.go
+++ pkg/test/harness.go
@@ -3,14 +3,12 @@ package test
 import (
 	"context"
 	"io/ioutil"
-	"log"
 	"math/rand"
 	"path/filepath"
 	"testing"
 	"time"
 
 	"github.com/kudobuilder/kudo/pkg/apis"
-	kudo "github.com/kudobuilder/kudo/pkg/apis/kudo/v1alpha1"
 	"github.com/kudobuilder/kudo/pkg/controller"
 	testutils "github.com/kudobuilder/kudo/pkg/test/utils"
 	"github.com/kudobuilder/kudo/pkg/webhook"
@@ -24,8 +22,12 @@ import (
 
 // Harness loads and runs tests based on the configuration provided.
 type Harness struct {
-	TestSuite kudo.TestSuite
-	T         *testing.T
+	T                 *testing.T
+	CRDDir            string
+	ManifestsDir      string
+	TestDirs          []string
+	StartControlPlane bool
+	StartKUDO         bool
 
 	managerStopCh chan struct{}
 	config        *rest.Config
@@ -85,7 +87,7 @@ func (h *Harness) Config() (*rest.Config, error) {
 
 	var err error
 
-	if h.TestSuite.StartControlPlane {
+	if h.StartControlPlane {
 		h.config, err = h.RunTestEnv()
 	} else {
 		h.config, err = config.GetConfig()
@@ -95,31 +97,44 @@ func (h *Harness) Config() (*rest.Config, error) {
 }
 
 // RunKUDO starts the KUDO controllers and installs the required CRDs.
-func (h *Harness) RunKUDO() error {
+func (h *Harness) RunKUDO(crdPath string) error {
 	config, err := h.Config()
 	if err != nil {
-		log.Printf("Error getting harness config: %v\n", err)
 		return err
+	}
+
+	if h.CRDDir != "" {
+		crds, err := envtest.InstallCRDs(config, envtest.CRDInstallOptions{
+			Paths:              []string{h.CRDDir},
+			ErrorIfPathMissing: true,
+		})
+		if err != nil {
+			return err
+		}
+
+		err = envtest.WaitForCRDs(config, crds, envtest.CRDInstallOptions{
+			Paths:              []string{h.CRDDir},
+			ErrorIfPathMissing: true,
+		})
+		if err != nil {
+			return err
+		}
 	}
 
 	mgr, err := manager.New(config, manager.Options{})
 	if err != nil {
-		log.Printf("Error creating manager: %v\n", err)
 		return err
 	}
 
-	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
-		log.Printf("Error adding scheme to manager: %v\n", err)
+	if err = apis.AddToScheme(mgr.GetScheme()); err != nil {
 		return err
 	}
 
 	if err = controller.AddToManager(mgr); err != nil {
-		log.Printf("Error adding manager to controller: %v\n", err)
 		return err
 	}
 
 	if err = webhook.AddToManager(mgr); err != nil {
-		log.Printf("Error getting harness config: %v\n", err)
 		return err
 	}
 
@@ -130,23 +145,17 @@ func (h *Harness) RunKUDO() error {
 }
 
 // Client returns the current Kubernetes client for the test harness.
-func (h *Harness) Client(forceNew bool) (client.Client, error) {
-	if h.client != nil && !forceNew {
+func (h *Harness) Client() (client.Client, error) {
+	if h.client != nil {
 		return h.client, nil
 	}
 
 	config, err := h.Config()
 	if err != nil {
-		log.Printf("Error getting harness config: %v\n", err)
 		return nil, err
 	}
 
-	h.client, err = client.New(config, client.Options{
-		Scheme: testutils.Scheme(),
-	})
-	if err != nil {
-		log.Printf("Error getting new client: %v\n", err)
-	}
+	h.client, err = client.New(config, client.Options{})
 	return h.client, err
 }
 
@@ -168,19 +177,19 @@ func (h *Harness) DiscoveryClient() (discovery.DiscoveryInterface, error) {
 // RunTests should be called from within a Go test (t) and launches all of the KUDO integration
 // tests at dir.
 func (h *Harness) RunTests() {
-	cl, err := h.Client(false)
+	cl, err := h.Client()
 	if err != nil {
 		h.T.Fatal(err)
 	}
 
-	dclient, err := h.DiscoveryClient()
+	dClient, err := h.DiscoveryClient()
 	if err != nil {
 		h.T.Fatal(err)
 	}
 
 	tests := []*Case{}
 
-	for _, testDir := range h.TestSuite.TestDirs {
+	for _, testDir := range h.TestDirs {
 		tempTests, err := h.LoadTests(testDir)
 		if err != nil {
 			h.T.Fatal(err)
@@ -191,7 +200,7 @@ func (h *Harness) RunTests() {
 	h.T.Run("harness", func(t *testing.T) {
 		for _, test := range tests {
 			test.Client = cl
-			test.DiscoveryClient = dclient
+			test.DiscoveryClient = dClient
 
 			if err := test.LoadTestSteps(); err != nil {
 				t.Fatal(err)
@@ -209,7 +218,13 @@ func (h *Harness) Run() {
 
 	defer h.Stop()
 
-	cl, err := h.Client(false)
+	if h.StartKUDO || h.StartControlPlane {
+		if err := h.RunKUDO("../../config/crds/"); err != nil {
+			h.T.Fatal(err)
+		}
+	}
+
+	cl, err := h.Client()
 	if err != nil {
 		h.T.Fatal(err)
 	}
@@ -219,31 +234,8 @@ func (h *Harness) Run() {
 		h.T.Fatal(err)
 	}
 
-	// Install CRDs
-	crds, err := testutils.InstallManifests(context.TODO(), cl, dClient, h.TestSuite.CRDDir)
-	if err != nil {
+	if err := testutils.InstallManifests(context.TODO(), cl, dClient, h.ManifestsDir); err != nil {
 		h.T.Fatal(err)
-	}
-
-	if err := testutils.WaitForCRDs(dClient, crds); err != nil {
-		h.T.Fatal(err)
-	}
-
-	// Create a new client to bust the client's CRD cache.
-	cl, err = h.Client(true)
-	if err != nil {
-		h.T.Fatal(err)
-	}
-
-	// Install required manifests.
-	if _, err := testutils.InstallManifests(context.TODO(), cl, dClient, h.TestSuite.ManifestsDir); err != nil {
-		h.T.Fatal(err)
-	}
-
-	if h.TestSuite.StartKUDO || h.TestSuite.StartControlPlane {
-		if err := h.RunKUDO(); err != nil {
-			h.T.Fatal(err)
-		}
 	}
 
 	h.RunTests()
