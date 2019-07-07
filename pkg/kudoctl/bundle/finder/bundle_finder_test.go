e2cbc3936ee130dc89273ae1133ad8ab0884f60c
kensipe@gmail.com <ken@mesosphere.io>
2019-07-07 16:04:00 -0500

    tests for bundle finder and http client

---
+++ pkg/kudoctl/bundle/finder/bundle_finder_test.go
@@ -0,0 +1,46 @@
+package finder
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestManager_GetBundle(t *testing.T) {
+	f := &Manager{
+		local: NewLocal(),
+		uri:   nil,
+	}
+	b, err := f.GetBundle("../testdata/zk", "")
+	if err != nil {
+		t.Errorf("Manager.GetBundle() error = %v", err)
+		return
+	}
+
+	crd, err := b.GetCRDs()
+	if err != nil {
+		t.Errorf("bundle.GetCRDs error = %v", err)
+	}
+	assert.EqualValues(t, "zookeeper", crd.Operator.Name)
+}
+
+func TestLocalFinder_GetBundle(t *testing.T) {
+	f := NewLocal()
+	b, err := f.GetBundle("../testdata/zk", "")
+	if err != nil {
+		t.Errorf("Manager.GetBundle() error = %v", err)
+		return
+	}
+
+	crd, err := b.GetCRDs()
+	if err != nil {
+		t.Errorf("bundle.GetCRDs error = %v", err)
+	}
+	assert.EqualValues(t, "zookeeper", crd.Operator.Name)
+}
+
+func TestLocalFinder_Failure(t *testing.T) {
+	f := NewLocal()
+	_, err := f.GetBundle("../testdata/zk-bad", "")
+	assert.Errorf(t, err, "should have errored on bad folder name")
+}
