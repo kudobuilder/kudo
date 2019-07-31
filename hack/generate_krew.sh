b7840d2630ed011894286a8e708c4057cffe4988
Justin Taylor-Barrick <46463088+jbarrick-mesosphere@users.noreply.github.com>
2019-07-30 17:41:38 -0700

    Add script for generating a krew plugin manifest. (#645)
    * Add script for generating a krew plugin manifest.
    
    * Add note about making PR to krew
    
    * improve plugin documentation
    
    * Fetch kudo plugin from github for hashing.
    
    * Update shortDescription
    
    * Add generate_krew.sh to RELEASE.md documentation.

---
+++ hack/generate_krew.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# This script generates a Krew-compatible plugin manifest. It should be run after goreleaser.
+
+VERSION=${VERSION:-$(git describe --tags |sed 's/^v//g')}
+
+# Generate the manifest for a single platform.
+function generate_platform {
+    ARCH="${2}"
+    if [ "${2}" == "amd64" ]; then
+        ARCH=x86_64
+    elif [ "${2}" == "386" ]; then
+        ARCH=i386
+    fi
+
+    cat <<EOF
+  - selector:
+      matchLabels:
+        os: "${1}"
+        arch: "${2}"
+    uri: https://github.com/kudobuilder/kudo/releases/download/v${VERSION}/kudo_${VERSION}_${1}_${ARCH}.tar.gz
+    sha256: "$(curl -L https://github.com/kudobuilder/kudo/releases/download/v${VERSION}/kudo_${VERSION}_${1}_${ARCH}.tar.gz |sha256sum - |awk '{print $1}')"
+    bin: "./kubectl-kudo"
+    files:
+    - from: "*"
+      to: "."
+EOF
+}
+
+rm -f kudo.yaml
+
+cat <<EOF >> kudo.yaml
+apiVersion: krew.googlecontainertools.github.com/v1alpha2
+kind: Plugin
+metadata:
+  name: kudo
+spec:
+  version: "${VERSION}"
+
+  shortDescription: Declaratively build, install, and run operators using KUDO.
+  homepage: https://kudo.dev/
+  description: |
+    The Kubernetes Universal Declarative Operator (KUDO) is a highly productive toolkit for writing operators for Kubernetes.
+    Using KUDO, you can deploy your applications, give your users the tools they need to operate it, and understand how it's
+    behaving in their environments — all without a PhD in Kubernetes. 
+
+  platforms:
+EOF
+
+generate_platform linux amd64 >> kudo.yaml
+generate_platform linux 386 >> kudo.yaml
+generate_platform darwin amd64 >> kudo.yaml
+generate_platform darwin 386 >> kudo.yaml
+generate_platform windows amd64 >> kudo.yaml
+generate_platform windows 386 >> kudo.yaml
+
+echo "To publish to the krew index, create a pull request to https://github.com/kubernetes-sigs/krew-index/tree/master/plugins to update kudo.yaml with the newly generated kudo.yaml."
