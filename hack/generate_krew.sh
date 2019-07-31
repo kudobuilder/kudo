77c34af795cfdeb61056e3c702fc53017cc560f1
jbarrick@mesosphere.com <jbarrick@mesosphere.com>
2019-07-30 17:48:22 -0700

    Fix EXE path when generating Krew plugin.

--- hack/generate_krew.sh
+++ hack/generate_krew.sh
@@ -20,7 +20,7 @@ function generate_platform {
         arch: "${2}"
     uri: https://github.com/kudobuilder/kudo/releases/download/v${VERSION}/kudo_${VERSION}_${1}_${ARCH}.tar.gz
     sha256: "$(curl -L https://github.com/kudobuilder/kudo/releases/download/v${VERSION}/kudo_${VERSION}_${1}_${ARCH}.tar.gz |sha256sum - |awk '{print $1}')"
-    bin: "./kubectl-kudo"
+    bin: "${3}"
     files:
     - from: "*"
       to: "."
@@ -47,11 +47,11 @@ spec:
   platforms:
 EOF
 
-generate_platform linux amd64 >> kudo.yaml
-generate_platform linux 386 >> kudo.yaml
-generate_platform darwin amd64 >> kudo.yaml
-generate_platform darwin 386 >> kudo.yaml
-generate_platform windows amd64 >> kudo.yaml
-generate_platform windows 386 >> kudo.yaml
+generate_platform linux amd64 ./kubectl-kudo >> kudo.yaml
+generate_platform linux 386 ./kubectl-kudo >> kudo.yaml
+generate_platform darwin amd64 ./kubectl-kudo >> kudo.yaml
+generate_platform darwin 386 ./kubectl-kudo >> kudo.yaml
+generate_platform windows amd64 ./kubectl-kudo.exe >> kudo.yaml
+generate_platform windows 386 ./kubectl-kudo.exe >> kudo.yaml
 
 echo "To publish to the krew index, create a pull request to https://github.com/kubernetes-sigs/krew-index/tree/master/plugins to update kudo.yaml with the newly generated kudo.yaml."
