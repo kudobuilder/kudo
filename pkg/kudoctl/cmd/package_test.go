c4bb85bd780cf3cee26843dd9eb6ac82c519509f
Ken Sipe <kensipe@gmail.com>
2019-08-02 17:32:52 -0500

    code clean up

--- pkg/kudoctl/cmd/package_test.go
+++ pkg/kudoctl/cmd/package_test.go
@@ -43,7 +43,6 @@ var bundleCmdArgs = []struct {
 
 func TestTableNewBundleCmd(t *testing.T) {
 	fs := afero.NewMemMapFs()
-	//fs := afero.NewOsFs()
 	copyOperatorToFs(fs, "../../../config/samples/first-operator")
 	for _, test := range bundleCmdArgs {
 		newCmdBundle := newPackageCmd(fs, os.Stdout)
