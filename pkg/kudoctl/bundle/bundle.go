d6b90902332045284f416daf960c33bea3bfe5d1
Ken Sipe <kensipe@gmail.com>
2019-08-06 11:55:50 -0500

    tarball code needs to use tarball writer

--- pkg/kudoctl/bundle/bundle.go
+++ pkg/kudoctl/bundle/bundle.go
@@ -108,55 +108,7 @@ func ToTarBundle(fs afero.Fs, path string, destination string, overwrite bool) (
 	}
 	defer file.Close()
 
-	gw := gzip.NewWriter(file)
-	defer gw.Close()
-	tw := tar.NewWriter(gw)
-	defer tw.Close()
-
-	err = afero.Walk(fs, path, func(file string, fi os.FileInfo, err error) error {
-
-		// return on any error
-		if err != nil {
-			return err
-		}
-
-		// return on non-regular files.  We don't add directories without files and symlinks
-		if !fi.Mode().IsRegular() {
-			return nil
-		}
-
-		// create a new dir/file header
-		header, err := tar.FileInfoHeader(fi, fi.Name())
-		if err != nil {
-			fmt.Printf("Error creating tar header for: %v", fi.Name())
-			return err
-		}
-
-		// update the name to correctly reflect the desired destination when untaring
-		header.Name = strings.TrimPrefix(strings.Replace(file, path, "", -1), string(filepath.Separator))
-
-		// write the header
-		if err := tw.WriteHeader(header); err != nil {
-			return err
-		}
-
-		// open files for taring
-		f, err := fs.Open(file)
-		if err != nil {
-			return err
-		}
-
-		// copy file data into tar writer
-		if _, err := io.Copy(tw, f); err != nil {
-			return err
-		}
-
-		// manually close here after each file operation; deferring would cause each file close
-		// to wait until all operations have completed.
-		f.Close()
-
-		return nil
-	})
+	err = tarballWriter(fs, path, file)
 	return target, err
 }
 
