111e3bb5471aacbf1c27634a752b28f5d85f5416
Ken Sipe <kensipe@gmail.com>
2019-09-05 16:12:58 -0500

    adding test for repo context

--- pkg/kudoctl/cmd/repo_context_test.go
+++ pkg/kudoctl/cmd/repo_context_test.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"bytes"
+	"github.com/kudobuilder/kudo/pkg/kudoctl/util/repo"
+	"os"
 	"testing"
 
 	"github.com/kudobuilder/kudo/pkg/kudoctl/kudohome"
@@ -35,4 +37,45 @@ func TestRepoContextInvalidArg(t *testing.T) {
 	cmd := repoContextCmd{name: "foo", home: home, fs: fs}
 	err = cmd.run()
 	assert.EqualError(t, err, `no repo named "foo" found`)
+}
+
+func TestRepoContextSwitch(t *testing.T) {
+	// There is a bug in afero I fought for some time.. results in a PathError golang.org/x/sys/unix.ENOENT (2)
+	// Issue is in the last write of the file in the cmd.run()
+	// everything in code looked correct and local manual testing worked.  Switching to temp folder
+	file := os.TempDir()
+	defer os.RemoveAll(file)
+
+	fs := afero.NewOsFs()
+	out := &bytes.Buffer{}
+
+	home := kudohome.Home(file)
+
+	// start with initialized client
+	i := &initCmd{fs: fs, out: out, home: home}
+	if err := i.initialize(); err != nil {
+		t.Error(err)
+	}
+
+	// add a repo config
+	repos := repo.NewRepositories()
+	fooConfig := &repo.Configuration{URL: "test", Name: "foo"}
+	repos.Add(fooConfig)
+
+	err := repos.WriteFile(fs, home.RepositoryFile(), 0644)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// test setting to it
+	cmd := repoContextCmd{name: "foo", home: home, fs: fs}
+	err = cmd.run()
+	assert.Nil(t, err)
+
+	// re-read repositories.yaml
+	repos, err = repo.LoadRepositories(fs, home.RepositoryFile())
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, repos.Context, "foo")
 }
