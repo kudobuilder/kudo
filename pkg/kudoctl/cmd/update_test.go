ea1231773314cf53586818396a066d3edb50059c
Ken Sipe <kensipe@gmail.com>
2019-09-10 16:56:22 -0500

    Global flag refactor

--- pkg/kudoctl/cmd/update_test.go
+++ pkg/kudoctl/cmd/update_test.go
@@ -4,6 +4,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/kudobuilder/kudo/pkg/kudoctl/env"
+
 	"github.com/kudobuilder/kudo/pkg/apis/kudo/v1alpha1"
 	util "github.com/kudobuilder/kudo/pkg/util/kudo"
 	v1 "k8s.io/api/core/v1"
@@ -76,10 +78,7 @@ func TestUpdate(t *testing.T) {
 			c.InstallInstanceObjToCluster(&testInstance, installNamespace)
 		}
 
-		err := update(testInstance.Name, c, &updateOptions{
-			Namespace:  installNamespace,
-			Parameters: tt.parameters,
-		})
+		err := update(testInstance.Name, c, &updateOptions{Parameters: tt.parameters}, env.DefaultSettings)
 		if err != nil {
 			if !strings.Contains(err.Error(), tt.errMessageContains) {
 				t.Errorf("%s: expected error '%s' but got '%v'", tt.name, tt.errMessageContains, err)
