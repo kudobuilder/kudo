f3e21f7f868cdcd4ebb6c587612aa1defedd2c3f
Ken Sipe <kensipe@gmail.com>
2019-09-10 15:20:13 -0500

    Bumping go version to 1.13 for builds and tests

--- build/test-images.sh
+++ build/test-images.sh
@@ -10,5 +10,8 @@ docker push kudobuilder/golang:1.11
 docker build --build-arg GOLANG_VERSION=1.12 -t kudobuilder/golang:1.12 .
 docker push kudobuilder/golang:1.12
 
+docker build --build-arg GOLANG_VERSION=1.13 -t kudobuilder/golang:1.13 .
+docker push kudobuilder/golang:1.13
+
 docker build --build-arg GOLANG_VERSION=latest -t kudobuilder/golang:latest .
 docker push kudobuilder/golang:latest
