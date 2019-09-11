26f4929a68125f8aff2532e7f7a8a43e87875a82
Jan Schlicht <jan@d2iq.com>
2019-09-11 10:03:59 +0200

    Lint shell scripts and ensure consistency
    These shell scripts have been linted with `shellcheck` and made consistent by
      * Using `bash` for all scripts
      * Indenting with 4 spaces
      * Having 'then' in the same line as 'if'
      * Using the same options for all scripts
      * Using `$(...)` notation instead of backticks
      * Removing `grep` when `awk` alone can do the job
    
    This also fixes an issue with `docker pull` retries not working in `test/run_tests.sh`.

--- hack/check_formatting.sh
+++ hack/check_formatting.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -x -e -o pipefail
+set -o errexit
+set -o nounset
+set -o pipefail
 
-ROOT=$(dirname "${BASH_SOURCE}")/..
+ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 PACKAGES="${ROOT}/pkg/ ${ROOT}/cmd/"
 
 # Make sure goimports doesn't find any errors
 go install golang.org/x/tools/cmd/goimports
 
 echo "goimports -d ${PACKAGES}"
-differences=`goimports -d ${PACKAGES}`
+# shellcheck disable=SC2086
+differences=$(goimports -d ${PACKAGES})
 if [[ ! "$differences" == "" ]]; then
     echo "goimports found the following differences"
     echo "$differences"
