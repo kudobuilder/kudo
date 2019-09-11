289be93e75b25ca00967c118a00ddabaaabd08a1
Jan Schlicht <jan@d2iq.com>
2019-09-11 14:55:07 +0200

    Lint shell scripts and ensure consistency (#793)
    These shell scripts have been linted with `shellcheck` and made consistent by
      * Using `bash` for all scripts
      * Indenting with 4 spaces
      * Having 'then' in the same line as 'if'
      * Using the same options for all scripts
      * Using `$(...)` notation instead of backticks
      * Removing `grep` when `awk` alone can do the job
    
    This also fixes an issue with `docker pull` retries not working in `test/run_tests.sh`.

--- hack/update_manifests.sh
+++ hack/update_manifests.sh
@@ -11,12 +11,11 @@ export GO111MODULE=on
 VERSION=$(go list -m all | grep sigs.k8s.io/controller-tools | rev | cut -d"-" -f1 | cut -d" " -f1 | rev)
 CONTROLLER_GEN_DIR="hack/controller-gen/$VERSION"
 
-if [ -d $CONTROLLER_GEN_DIR ]   # for file "if [-f /home/rama/file]"
-then
+if [ -d "${CONTROLLER_GEN_DIR}" ]; then
     echo "Using cached controller generator version: $VERSION"
 else
-  git clone https://github.com/kubernetes-sigs/controller-tools.git "${CONTROLLER_GEN_DIR}"
-  (cd "${CONTROLLER_GEN_DIR}" && git reset --hard "${VERSION}" && go mod init)
+    git clone https://github.com/kubernetes-sigs/controller-tools.git "${CONTROLLER_GEN_DIR}"
+    (cd "${CONTROLLER_GEN_DIR}" && git reset --hard "${VERSION}" && go mod init)
 fi
 
 go run "$CONTROLLER_GEN_DIR"/cmd/controller-gen/main.go rbac
--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 # Pull the builder image with retries if it doesn't already exist.
 retries=0
-builder_image=$(grep FROM test/Dockerfile | awk '{print $2}')
+builder_image=$(awk '/FROM/ {print $2}' test/Dockerfile)
 
 if ! docker inspect "$builder_image"; then
     until docker pull "$builder_image"; do
-        if [ retries -eq 3 ]; then
+        if [ $retries -eq 3 ]; then
             echo "Giving up downloading builder image, failing build."
             exit 1
         fi
@@ -16,20 +20,13 @@ if ! docker inspect "$builder_image"; then
     done
 fi
 
-docker build -f test/Dockerfile -t kudo-test .
-if [ $? -eq 0 ]
-then
-   docker run -it -m 4g -v $(pwd)/reports:/go/src/github.com/kudobuilder/kudo/reports --rm kudo-test
+if docker build -f test/Dockerfile -t kudo-test .; then
+    if docker run -it -m 4g -v "$(pwd)"/reports:/go/src/github.com/kudobuilder/kudo/reports --rm kudo-test; then
+        echo "Tests finished successfully! ヽ(•‿•)ノ"
+    else
+        exit $?
+    fi
 else
     echo "Error when building test docker image, cannot run tests."
     exit 1
 fi
-
-DOCKER_EXIT_CODE=$?
-
-if [ $DOCKER_EXIT_CODE -eq 0 ]
-then
-   echo "Tests finished successfully! ヽ(•‿•)ノ"
-fi
-
-exit $DOCKER_EXIT_CODE
