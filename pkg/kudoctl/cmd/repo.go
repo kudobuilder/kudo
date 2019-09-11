c4d9c135c4d71f52f7bcf0034313f7a7efe37387
Ken Sipe <kensipe@gmail.com>
2019-09-11 11:28:51 -0500

    Fixing repo command short message (#787)

--- pkg/kudoctl/cmd/repo.go
+++ pkg/kudoctl/cmd/repo.go
@@ -8,19 +8,24 @@ import (
 )
 
 const repoDesc = `
-	This command consists of multiple sub-commands to interact with KUDO repositories.
-	
-	It can be used to add, remove, list, and index kudo repositories.
-	Example usage:
-		$ kubectl kudo repo add [NAME] [REPO_URL]`
+This command consists of multiple sub-commands to interact with KUDO repositories.
+
+It can be used to add, remove, list, and index kudo repositories.
+`
+
+const examples = `  kubectl kudo repo add [NAME] [REPO_URL]
+  kubectl kudo repo remmove
+  kubectl kudo repo list
+  kubectl kudo repo context [NAME]
+`
 
 // newRepoCmd for repo commands such as building a repo index
 func newRepoCmd(fs afero.Fs, out io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "repo [FLAGS] add|remove|list|index| update [ARGS]",
-		Short:   "Package an official KUDO operator.",
-		Long:    `Add, list, remove, update, and index kudo repositories`,
-		Example: repoDesc,
+		Use:     "repo [FLAGS] add|remove|list|index [ARGS]",
+		Short:   "Add, list, remove, and index kudo repositories.",
+		Long:    repoDesc,
+		Example: examples,
 	}
 
 	cmd.AddCommand(newRepoIndexCmd(fs, out))
