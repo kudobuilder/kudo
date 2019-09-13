a314f73f58c6d4181906baf0fafc228fe11f18ca
Gerred Dillon <hello@gerred.org>
2019-09-13 11:57:41 -0400

    remove inline action

--- .github/actions/current-time/index.js
+++
@@ -1,9 +0,0 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-
-try {
-  const time = new Date().toISOString();
-  core.setOutput("time", time);
-} catch (error) {
-  core.setFailed(error.message);
-}
