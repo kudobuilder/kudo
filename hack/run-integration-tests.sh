ccc0cf1ecb51052a9d2ff3159234da89cfa2387e
Alena Varkockova <varkockova.a@gmail.com>
2019-10-15 10:14:37 +0200

    Some script fixing

--- hack/run-integration-tests.sh
+++ hack/run-integration-tests.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
-if [ "INTEGRATION_OUTPUT_JUNIT" = "true" ]
+set -o errexit
+set -o nounset
+set -o pipefail
+
+INTEGRATION_OUTPUT_JUNIT=${INTEGRATION_OUTPUT_JUNIT:-false}
+
+if [ "$INTEGRATION_OUTPUT_JUNIT" == true ]
 then
+    echo "Running tests with junit output"
     mkdir -p reports/
     go get github.com/jstemmer/go-junit-report
     go test -tags integration ./pkg/... ./cmd/... -v -mod=readonly -coverprofile cover-integration.out 2>&1 |tee /dev/fd/2 |go-junit-report -set-exit-code > reports/integration_report.xml
     go run ./cmd/kubectl-kudo test 2>&1 |tee /dev/fd/2 |go-junit-report -set-exit-code > reports/kudo_test_report.xml
 else
+    echo "Running tests without junit output"
     go test -tags integration ./pkg/... ./cmd/... -v -mod=readonly -coverprofile cover-integration.out
     go run ./cmd/kubectl-kudo test
 fi
