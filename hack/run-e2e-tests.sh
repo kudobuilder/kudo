87a324ef9457d80369a21060ad368216f8dd6458
Jan Schlicht <jan@d2iq.com>
2019-10-17 10:55:26 +0200

    Distinguish between local and operator E2E tests
    Tests in the 'test/e2e' folder are run as part of the E2E tests in addition
    to the operator tests.

--- hack/run-e2e-tests.sh
+++ hack/run-e2e-tests.sh
@@ -11,13 +11,19 @@ then
     echo "Running E2E tests with junit output"
     mkdir -p reports/
     go get github.com/jstemmer/go-junit-report
+
+    go run ./cmd/kubectl-kudo test --config kudo-e2e-test.yaml 2>&1 |tee /dev/fd/2 |go-junit-report -set-exit-code > reports/kudo_e2e_test_report.xml
+
 	rm -rf operators
 	git clone https://github.com/kudobuilder/operators
 	mkdir operators/bin/
 	cp ./bin/kubectl-kudo operators/bin/
-	cd operators && go run ../cmd/kubectl-kudo test 2>&1 |tee /dev/fd/2 |go-junit-report -set-exit-code > ../reports/kudo_e2e_test_report.xml
+	cd operators && go run ../cmd/kubectl-kudo test 2>&1 |tee /dev/fd/2 |go-junit-report -set-exit-code > ../reports/kudo_operators_test_report.xml
 else
     echo "Running E2E tests without junit output"
+
+    go run ./cmd/kubectl-kudo test --config kudo-e2e-test.yaml
+
 	rm -rf operators
 	git clone https://github.com/kudobuilder/operators
 	mkdir operators/bin/
