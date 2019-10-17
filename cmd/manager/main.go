595a70a47ed160b3e6398a5db5169f70f4bb785c
Alena Varkockova <varkockova.a@gmail.com>
2019-10-17 10:36:21 +0200

    Missing fixes

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -23,6 +23,7 @@ import (
 	"github.com/kudobuilder/kudo/pkg/controller/instance"
 	"github.com/kudobuilder/kudo/pkg/controller/operator"
 	"github.com/kudobuilder/kudo/pkg/controller/operatorversion"
+	util "github.com/kudobuilder/kudo/pkg/test/utils"
 	"github.com/kudobuilder/kudo/pkg/version"
 	apiextenstionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 
@@ -40,7 +41,9 @@ func main() {
 
 	// create new controller-runtime manager
 	log.Info("setting up manager")
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{})
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		MapperProvider: util.NewDynamicRESTMapper,
+	})
 	if err != nil {
 		log.Error(err, "unable to start manager")
 		os.Exit(1)
