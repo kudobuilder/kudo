a75f200192b6a6ac95a7b4d53bf326a6e5cf8e90
Sivaram Kannan <sivaramsk@gmail.com>
2019-11-22 19:09:11 +0530

    Change the new command line flat inline with the convention from serviceaccount to serviceAccount.

--- pkg/kudoctl/cmd/init.go
+++ pkg/kudoctl/cmd/init.go
@@ -61,7 +61,7 @@ type initCmd struct {
 	output         string
 	version        string
 	ns             string
-	serviceaccount string
+	serviceAccount string
 	wait           bool
 	timeout        int64
 	clientOnly     bool
@@ -101,7 +101,7 @@ func newInitCmd(fs afero.Fs, out io.Writer) *cobra.Command {
 	f.BoolVar(&i.crdOnly, "crd-only", false, "Add only KUDO CRDs to your cluster")
 	f.BoolVarP(&i.wait, "wait", "w", false, "Block until KUDO manager is running and ready to receive requests")
 	f.Int64Var(&i.timeout, "wait-timeout", 300, "Wait timeout to be used")
-	f.StringVarP(&i.serviceaccount, "service-account", "", "", "Override for the default serviceAccount kudo-manager")
+	f.StringVarP(&i.serviceAccount, "service-account", "", "", "Override for the default serviceAccount kudo-manager")
 
 	return cmd
 }
@@ -128,7 +128,7 @@ func (initCmd *initCmd) validate(flags *flag.FlagSet) error {
 
 // run initializes local config and installs KUDO manager to Kubernetes cluster.
 func (initCmd *initCmd) run() error {
-	opts := cmdInit.NewOptions(initCmd.version, initCmd.ns, initCmd.serviceaccount)
+	opts := cmdInit.NewOptions(initCmd.version, initCmd.ns, initCmd.serviceAccount)
 	// if image provided switch to it.
 	if initCmd.image != "" {
 		opts.Image = initCmd.image
