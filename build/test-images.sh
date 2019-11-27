49002e995cba10685b0d61f46a368f3565f4cb18
Marcin Owsiany <marcin@owsiany.pl>
2019-11-27 11:19:28 +0100

    Stop creating old test images. (#1102)
    We no longer support Golang < 1.13 nor use these images.

--- build/test-images.sh
+++ build/test-images.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-
-docker build --build-arg GOLANG_VERSION=1.10 -t kudobuilder/golang:1.10 .
-docker push kudobuilder/golang:1.10
-
-docker build --build-arg GOLANG_VERSION=1.11 -t kudobuilder/golang:1.11 .
-docker push kudobuilder/golang:1.11
-
-docker build --build-arg GOLANG_VERSION=1.12 -t kudobuilder/golang:1.12 .
-docker push kudobuilder/golang:1.12
-
 docker build --build-arg GOLANG_VERSION=1.13 -t kudobuilder/golang:1.13 .
 docker push kudobuilder/golang:1.13
 
