1e9e07dbbf1718061cd54c33cee8b1001b842efe
Andreas Neumann <aneumann@mesosphere.com>
2019-12-02 11:27:02 +0100

    Validate KUDO CRDs before running commands (#1113)
    * Verify that the correct and expected CRDs are installed before executing any commands
    * Added parameter to ignore the validation

--- pkg/kudoctl/util/kudo/kudo_test.go
+++ pkg/kudoctl/util/kudo/kudo_test.go
@@ -19,6 +19,19 @@ func newTestSimpleK2o() *Client {
 	return NewClientFromK8s(fake.NewSimpleClientset())
 }
 
+func TestKudoClientValidate(t *testing.T) {
+	tests := []struct {
+		err string
+	}{
+		{"CRDs invalid: failed to retrieve CRD"}, // verify that NewClient tries to validate CRDs
+	}
+
+	for _, tt := range tests {
+		_, err := NewClient("testdata/test-config", 0, true)
+		assert.ErrorContains(t, err, tt.err)
+	}
+}
+
 func TestNewK2oClient(t *testing.T) {
 	tests := []struct {
 		err string
@@ -28,7 +41,7 @@ func TestNewK2oClient(t *testing.T) {
 
 	for _, tt := range tests {
 		// Just interested in errors
-		_, err := NewClient("", 0)
+		_, err := NewClient("", 0, false)
 		assert.ErrorContains(t, err, tt.err)
 	}
 }
