05144b95b476e7719fb014356d5fe82e72377d4f
Ken Sipe <kensipe@gmail.com>
2019-12-27 09:41:01 -0600

    regen code.  these classes must have had their imports organized... which was inappropriate for code gen classes
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

--- pkg/client/informers/externalversions/factory.go
+++ pkg/client/informers/externalversions/factory.go
@@ -21,14 +21,13 @@ import (
 	sync "sync"
 	time "time"
 
+	versioned "github.com/kudobuilder/kudo/pkg/client/clientset/versioned"
+	internalinterfaces "github.com/kudobuilder/kudo/pkg/client/informers/externalversions/internalinterfaces"
+	kudo "github.com/kudobuilder/kudo/pkg/client/informers/externalversions/kudo"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	cache "k8s.io/client-go/tools/cache"
-
-	versioned "github.com/kudobuilder/kudo/pkg/client/clientset/versioned"
-	internalinterfaces "github.com/kudobuilder/kudo/pkg/client/informers/externalversions/internalinterfaces"
-	kudo "github.com/kudobuilder/kudo/pkg/client/informers/externalversions/kudo"
 )
 
 // SharedInformerOption defines the functional option type for SharedInformerFactory.
