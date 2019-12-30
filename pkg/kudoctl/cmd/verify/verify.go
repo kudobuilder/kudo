3fa17aa1992416f12aa9b9cd5b7831a2e27fda32
Ken Sipe <kensipe@gmail.com>
2019-12-30 09:11:18 -0600

    refactored based on PR feedback and provided tests
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

--- pkg/kudoctl/cmd/verify/verify.go
+++ pkg/kudoctl/cmd/verify/verify.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/kudobuilder/kudo/pkg/kudoctl/packages"
 	"github.com/kudobuilder/kudo/pkg/kudoctl/packages/verifier"
+	"github.com/kudobuilder/kudo/pkg/kudoctl/packages/verifier/task"
 	"github.com/kudobuilder/kudo/pkg/kudoctl/packages/verifier/template"
 )
 
 var verifiers = []verifier.PackageVerifier{
 	DuplicateVerifier{},
 	InvalidCharVerifier{";,"},
-	PlanVerifier{},
+	task.ReferenceVerifier{},
 	template.ParametersVerifier{},
 	template.ReferenceVerifier{},
 }
@@ -56,65 +57,6 @@ func (v InvalidCharVerifier) Verify(pf *packages.Files) verifier.Result {
 			}
 		}
 
-	}
-
-	return res
-}
-
-// PlanVerifier verifies plans, with errors for step tasks that do not exist and warnings for tasks which are not used in a plan
-type PlanVerifier struct{}
-
-func (PlanVerifier) Verify(pf *packages.Files) verifier.Result {
-	res := verifier.NewResult()
-	res.Merge(tasksNotDefined(pf))
-	res.Merge(tasksDefinedNotUsed(pf))
-
-	return res
-}
-
-func tasksDefinedNotUsed(pf *packages.Files) verifier.Result {
-	res := verifier.NewResult()
-	usedTasks := make(map[string]bool)
-
-	// map / set of all tasks that are referenced in a plan
-	for _, plan := range pf.Operator.Plans {
-		for _, phase := range plan.Phases {
-			for _, step := range phase.Steps {
-				for _, task := range step.Tasks {
-					usedTasks[task] = true
-				}
-			}
-		}
-	}
-
-	for _, task := range pf.Operator.Tasks {
-		if _, ok := usedTasks[task.Name]; !ok {
-			res.AddWarnings(fmt.Sprintf("task %q defined but not used", task.Name))
-		}
-	}
-
-	return res
-}
-
-func tasksNotDefined(pf *packages.Files) verifier.Result {
-	res := verifier.NewResult()
-	definedTasks := make(map[string]bool)
-
-	// map of all tasks defined
-	for _, task := range pf.Operator.Tasks {
-		definedTasks[task.Name] = true
-	}
-
-	for planName, plan := range pf.Operator.Plans {
-		for _, phase := range plan.Phases {
-			for _, step := range phase.Steps {
-				for _, task := range step.Tasks {
-					if _, ok := definedTasks[task]; !ok {
-						res.AddErrors(fmt.Sprintf("task %q in plan %q is not defined", task, planName))
-					}
-				}
-			}
-		}
 	}
 
 	return res
