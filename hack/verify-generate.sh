72c1c600ece4fd07a80e2dc80c4284c87f592a71
Alena Varkockova <varkockova.a@gmail.com>
2019-12-31 12:47:46 +0100

    Update

--- hack/verify-generate.sh
+++ hack/verify-generate.sh
@@ -12,12 +12,12 @@ if [[ ${RETVAL} != 0 ]]; then
     exit 1
 fi
 
-git diff-index --quiet HEAD --
+git diff --exit-code --quiet
 RETVAL=$?
 
 if [[ ${RETVAL} != 0 ]]; then
     echo "Running 'make generate' produces changes to the current git status. Maybe you forgot to check-in your updated generated files?"
-    echo "The current diff: `git diff-index HEAD --`"
+    echo "The current diff: `git diff`"
     exit 1
 fi
 
