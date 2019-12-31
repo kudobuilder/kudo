f03a6cc3616a89f2d8687d14c3f8ea4d3147c8f5
Alena Varkockova <varkockova.a@gmail.com>
2019-12-31 11:14:17 +0100

    Introduce checking script

---
+++ hack/verify-generate.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# make sure make generate can be invoked
+if ! make generate; then
+    echo "Invoking 'make generate' ends with non-zero exit code."
+    exit 1
+fi
+
+if ! git diff-index --quiet HEAD --; then
+    echo "Running 'make generate' produces changes to the current git status. Maybe you forgot to check-in your updated generated files?"
+    exit 1
+fi
+
+echo "Verifying 'make generate' was successful! ヽ(•‿•)ノ"
+exit 0
