dd4880d6bd4bcbe0b68dcb4c86a37d829a12eae2
Jan Schlicht <jan@d2iq.com>
2020-01-07 12:39:50 +0100

    Store KinD logs artifact in E2E tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,11 +3,15 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -o xtrace
 
 # "TARGET" is a Makefile target that runs tests
 TARGET=$1
 
 INTEGRATION_OUTPUT_JUNIT=${INTEGRATION_OUTPUT_JUNIT:-false}
+
+# Set test harness artifacts dir to '/tmp/kudo-e2e-test', as it's easier to copy from in a container.
+echo 'artifactsDir: /tmp/kudo-e2e-test' >> kudo-e2e-test.yaml
 
 # Pull the builder image with retries if it doesn't already exist.
 retries=0
@@ -26,10 +30,24 @@ if ! docker inspect "$builder_image"; then
 fi
 
 if docker build -f test/Dockerfile -t kudo-test .; then
-    if docker run -e INTEGRATION_OUTPUT_JUNIT --net=host -it -m 4g -v /var/run/docker.sock:/var/run/docker.sock -v "$(pwd)"/reports:/go/src/github.com/kudobuilder/kudo/reports --rm kudo-test make "$TARGET"; then
+    docker run -e INTEGRATION_OUTPUT_JUNIT --net=host -it -m 4g \
+        --name kudo-e2e-test \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v "$(pwd)"/reports:/go/src/github.com/kudobuilder/kudo/reports \
+        kudo-test make "$TARGET"
+    RESULT=$?
+
+    # Archive test harness artifacts
+    if [ "$TARGET" == "e2e-test" ]; then
+        docker cp kudo-e2e-test:/tmp/kudo-e2e-test - | bzip2 > kind-logs.tar.bz2
+    fi
+
+    docker rm kudo-e2e-test
+
+    if [ $RESULT -eq 0 ]; then
         echo "Tests finished successfully! ヽ(•‿•)ノ"
     else
-        exit $?
+        exit $RESULT
     fi
 else
     echo "Error when building test docker image, cannot run tests."
