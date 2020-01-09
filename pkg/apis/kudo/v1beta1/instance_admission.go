fc08e53310b4c473ec2f5bfa626b411908c64853
Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>
2020-01-09 15:12:02 +0100

    Wip instance admission webhook pt. 4
    All tests are green and the rules implemented. However, the implementation became complex and brittle.

--- pkg/apis/kudo/v1beta1/instance_admission.go
+++ pkg/apis/kudo/v1beta1/instance_admission.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"reflect"
 
 	"github.com/thoas/go-funk"
 	"k8s.io/api/admission/v1beta1"
@@ -57,7 +56,9 @@ func (ia *InstanceAdmission) Handle(ctx context.Context, req admission.Request) 
 		}
 
 		// Populate Instance.PlanExecution with the plan triggered by param change
-		new.Spec.PlanExecution.PlanName = triggered
+		if triggered != nil {
+			new.Spec.PlanExecution.PlanName = *triggered
+		}
 
 		// PROFIT!
 		return admission.Allowed("")
@@ -66,55 +67,95 @@ func (ia *InstanceAdmission) Handle(ctx context.Context, req admission.Request) 
 	}
 }
 
-func validateUpdate(old, new *Instance, ov *OperatorVersion) (string, error) {
-	// Prereqs:
+// validateUpdate takes in the old and new (updated) instance and returns a new plan that might
+// be triggered based on the update and an error if the update is not valid. Return plan might be
+// - nil when there is no change to an existing scheduled plan
+// - "" empty string when an existing plan should be canceled (not implemented yet)
+// - "newPlan" some new plan that should be triggered
+func validateUpdate(old, new *Instance, ov *OperatorVersion) (*string, error) {
+	// PREREQS:
 	//  a) new PE (Instance.Spec.PlanExecution.PlanName)
 	//  b) new Params (parameterDiff)
 	//  c) new OV (Instance.Spec.OperatorVersion)
 	newPlan := new.Spec.PlanExecution.PlanName
 	oldPlan := old.Spec.PlanExecution.PlanName
-	paramDiff := parameterDiff(old.Spec.Parameters, new.Spec.Parameters)
 	newOvRef := new.Spec.OperatorVersion
 	oldOvRef := old.Spec.OperatorVersion
 
-	// DECLINE if:
-	// 1. old PE exists and != new PE : no plan overriding yet
+	paramDiff := parameterDiff(old.Spec.Parameters, new.Spec.Parameters)
+	log.Printf(">>> paramDiff: %v", paramDiff)
+
+	//***********************************
+	//****** ACCEPT/DECLINE RULES *******
+	//***********************************
+
+	isUpgrade := newOvRef != oldOvRef
+	isParameterUpdate := len(paramDiff) > 0
+
+	// === DIRECTLY TRIGGERED PLANS ===
+	// 1. decline: old PE exists and != new PE and there is no plan overriding yet
 	if oldPlan != "" && oldPlan != newPlan {
-		return "", fmt.Errorf("failed to update Instance %s/%s: plan '%s' is scheduled and an update would trigger a different plan '%s'", old.Namespace, old.Name, oldPlan, newPlan)
+		return nil, fmt.Errorf("failed to update Instance %s/%s: plan '%s' is scheduled and an update would trigger a different plan '%s'", old.Namespace, old.Name, oldPlan, newPlan)
 	}
 
-	// 2 OV changed and old PE exists : no upgrade if a plan running/scheduled
-	if oldPlan != "" && newOvRef != oldOvRef {
-		return "", fmt.Errorf("failed to update Instance %s/%s: upgrade to new OperatorVersion %s is not possible while a plan '%s' is scheduled", old.Namespace, old.Name, newOvRef, oldPlan)
+	// 2. allow: if NO plan is currently scheduled and this is not an upgrade
+	if oldPlan == "" && newPlan != "" && !isUpgrade && !isParameterUpdate {
+		return &newPlan, nil
 	}
 
-	// 3. OV changed and newPlan set : an upgrade should not be triggered with another plan
-	if newPlan != "" && newOvRef != oldOvRef {
-		return "", fmt.Errorf("failed to update Instance %s/%s: upgrade to new OperatorVersion %s is not possible together with a new plan '%s'", old.Namespace, old.Name, newOvRef, newPlan)
+	// === UPGRADES ===
+	// 3. decline: OV changed but an old plan is already scheduled
+	if oldPlan != "" && isUpgrade {
+		return nil, fmt.Errorf("failed to update Instance %s/%s: upgrade to new OperatorVersion %s is not possible while a plan '%s' is scheduled", old.Namespace, old.Name, newOvRef, oldPlan)
 	}
 
-	// 3.1 TODO (AD): No upgrade and no parameter diff detected : ???
+	// 4. decline: OV changed and newPlan set but an upgrade should not be triggered with another plan
+	if newPlan != "" && isUpgrade {
+		return nil, fmt.Errorf("failed to update Instance %s/%s: upgrade to new OperatorVersion %s is not possible together with a new plan '%s'", old.Namespace, old.Name, newOvRef, newPlan)
+	}
 
-	// 4. If >1 distinct plans are triggered based on params diff
+	// 5. allow: OV changed so we trigger an upgrade/update/deploy
+	if newPlan == "" && isUpgrade {
+		plan := selectPlan([]string{UpgradePlanName, UpdatePlanName, DeployPlanName}, ov)
+		if plan == nil {
+			return nil, fmt.Errorf("failed to update Instance %s/%s: couldn't find any suitable plan that would be triggered by an OperatorVersion upgrade", old.Namespace, old.Name)
+		}
+		return plan, nil
+	}
+
+	// === PARAMETER CHANGES ===
+	// 6. decline: if >1 distinct plans are triggered or no plan could be found based on the param diff
 	paramDefs := getParamDefinitions(paramDiff, ov)
 	triggered, err := parameterDiffPlan(paramDefs, ov)
 	if err != nil {
-		return "", fmt.Errorf("failed to update Instance %s/%s: %v", old.Namespace, old.Name, err)
+		return nil, fmt.Errorf("failed to update Instance %s/%s: %v", old.Namespace, old.Name, err)
 	}
 
-	// 5. If old PE != plan triggered by param change
-	if oldPlan != "" && triggered != oldPlan {
-		return "", fmt.Errorf("failed to update Instance %s/%s: plan '%s' is scheduled and an update would trigger a different plan '%s'", old.Namespace, old.Name, oldPlan, triggered)
+	// 7. We consider different transitions from oldPlan -> newPlan given that one/both might exist. Here, the order
+	// is important: case a) should take precedence over case c)
+	switch {
+	// a) allow: we have a plan scheduled and no actual change: this is a basically a noop
+	case triggered == "":
+		return nil, nil
+
+	// b) allow: if an already running plan is re-triggered by a parameter change,
+	// we reset the Instance.Status to effectively restart the plan
+	case oldPlan != "" && triggered == oldPlan:
+		// TODO (AD): reset the Instance.Status
+		return nil, nil
+
+	// c) decline: we already have a scheduled plan and decline the new one (forced plans
+	// executions might change the future)
+	case oldPlan != "" && triggered != oldPlan:
+		return nil, fmt.Errorf("failed to update Instance %s/%s: plan '%s' is scheduled and an update would trigger a different plan '%s'", old.Namespace, old.Name, oldPlan, triggered)
+
+	// d) decline: a parameter update and a different directly triggered plan are nto allowed
+	case newPlan != "" && triggered != newPlan:
+		return nil, fmt.Errorf("failed to update Instance %s/%s: triggering different plans directly '%s' and trhough a parameter update '%s' is not allowed", old.Namespace, old.Name, newPlan, triggered)
 	}
 
-	// if an already running plan is re-triggered by a parameter change, we reset the Instance.Status to
-	// effectively restart the plan
-	if oldPlan != "" && triggered == oldPlan {
-		// TODO (av): reset the Instance.Status
-	}
-
-	// else ACCEPT and return the triggered plan
-	return triggered, nil
+	// 8. accept: and return the triggered plan
+	return &triggered, nil
 }
 
 // getOperatorVersion retrieves operator version belonging to the given instance
@@ -162,10 +203,6 @@ func parameterDiffPlan(params []Parameter, ov *OperatorVersion) (string, error) 
 	default:
 		return "", fmt.Errorf("triggering multiple plans: [%v] at once is not allowed", plans)
 	}
-}
-
-func specChanged(old InstanceSpec, new InstanceSpec) bool {
-	return !reflect.DeepEqual(old, new)
 }
 
 // InstanceAdmission implements inject.Client.
--- pkg/apis/kudo/v1beta1/instance_admission_test.go
+++ pkg/apis/kudo/v1beta1/instance_admission_test.go
@@ -1,6 +1,7 @@
 package v1beta1
 
 import (
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,6 +12,10 @@ import (
 )
 
 func TestValidateUpdate(t *testing.T) {
+	// because Go doesn't let us take a pointer of a literal
+	deploy := "deploy"
+	update := "update"
+
 	ov := &OperatorVersion{
 		ObjectMeta: metav1.ObjectMeta{Name: "foo-operator", Namespace: "default"},
 		TypeMeta:   metav1.TypeMeta{Kind: "OperatorVersion", APIVersion: "kudo.dev/v1beta1"},
@@ -18,14 +23,22 @@ func TestValidateUpdate(t *testing.T) {
 			Plans: map[string]Plan{"deploy": {}, "update": {}},
 			Parameters: []Parameter{
 				{
-					Name:    "param",
-					Default: kudo.String("default"),
+					Name:    "foo",
+					Trigger: deploy,
+				},
+				{
+					Name:    "other-foo",
+					Trigger: deploy,
+				},
+				{
+					Name:    "bar",
+					Trigger: update,
 				},
 			},
 		},
 	}
 
-	runningInstance := Instance{
+	idle := &Instance{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "kudo.dev/v1beta1",
 			Kind:       "Instance",
@@ -38,53 +51,225 @@ func TestValidateUpdate(t *testing.T) {
 			OperatorVersion: v1.ObjectReference{
 				Name: "foo-operator",
 			},
-		},
-		Status: InstanceStatus{
-			AggregatedStatus: AggregatedStatus{
-				Status:         ExecutionInProgress,
-				ActivePlanName: "deploy",
+			Parameters: map[string]string{
+				"foo": "foo",
 			},
 		},
+		Status: InstanceStatus{},
 	}
 
+	scheduled := idle.DeepCopy()
+	scheduled.Spec.PlanExecution = PlanExecution{PlanName: deploy}
+
+	upgraded := idle.DeepCopy()
+	upgraded.Spec.OperatorVersion = v1.ObjectReference{Name: "foo-operator-2.0"}
+
 	tests := []struct {
-		name          string
-		new           Instance
-		old           Instance
-		ov            *OperatorVersion
-		want          string
-		expectedError error
+		name    string
+		new     *Instance
+		old     *Instance
+		ov      *OperatorVersion
+		want    *string
+		wantErr bool
 	}{
 		{
-			name: "no change",
-			new:  runningInstance,
-			old:  runningInstance,
+			name: "no change is a noop",
+			old:  idle,
+			new:  idle,
 			ov:   ov,
-			want: "",
 		},
 		{
-			name: "change in labels is allowed on running instance",
-			new:  runningInstance,
-			old: func() Instance {
-				updatedInstance := runningInstance.DeepCopy()
-				updatedInstance.ObjectMeta.Labels = map[string]string{"label": "label2"}
-				return *updatedInstance
+			name: "change in labels does not trigger a plan",
+			old:  scheduled,
+			new: func() *Instance {
+				i := scheduled.DeepCopy()
+				i.ObjectMeta.Labels = map[string]string{"label": "label2"}
+				return i
 			}(),
 			ov:   ov,
-			want: "",
+			want: nil,
 		},
-		//{name: "change in spec is not allowed on running instance", new: runningInstance, old: func() Instance {
-		//	updatedInstance := runningInstance.DeepCopy()
-		//	updatedInstance.Spec.Parameters = map[string]string{"newparam": "newvalue"}
-		//	return *updatedInstance
-		//}(), ov: ov, expectedError: errors.New("cannot update Instance test/test right now, there's plan deploy in progress")},
+		{
+			name: "triggering a plan directly IS allowed when NO plan is scheduled",
+			old:  idle,
+			new:  scheduled,
+			ov:   ov,
+			want: &deploy,
+		},
+		{
+			name: "triggering the same plan directly IS allowed",
+			old:  scheduled,
+			new:  scheduled,
+			ov:   ov,
+			want: nil,
+		},
+		{
+			name: "overriding an existing plan directly is NOT allowed",
+			old:  scheduled,
+			new: func() *Instance {
+				i := scheduled.DeepCopy()
+				i.Spec.PlanExecution = PlanExecution{PlanName: "update"}
+				return i
+			}(),
+			ov:      ov,
+			wantErr: true,
+		},
+		{
+			name: "canceling an existing plan directly is NOT allowed",
+			old:  scheduled,
+			new: func() *Instance {
+				i := scheduled.DeepCopy()
+				i.Spec.PlanExecution = PlanExecution{PlanName: ""}
+				return i
+			}(),
+			ov:      ov,
+			wantErr: true,
+		},
+		{
+			name: "upgrade triggered on an idle instance IS allowed",
+			old:  idle,
+			new:  upgraded,
+			ov:   ov,
+			want: &update, // 'update' is a fallback plan when 'upgrade' does not exist
+		},
+		{
+			name:    "upgrade triggered on a scheduled instance IS NOT allowed",
+			old:     scheduled,
+			new:     upgraded,
+			ov:      ov,
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "upgrade triggered on an idle instance together with another plan IS NOT allowed",
+			old:  idle,
+			new: func() *Instance {
+				i := upgraded.DeepCopy()
+				i.Spec.PlanExecution = PlanExecution{PlanName: deploy}
+				return i
+			}(),
+			ov:      ov,
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "parameter update on an idle instance IS allowed",
+			old:  idle,
+			new: func() *Instance {
+				i := idle.DeepCopy()
+				i.Spec.Parameters = map[string]string{"foo": "newFoo"}
+				return i
+			}(),
+			ov:   ov,
+			want: &deploy,
+		},
+		{
+			name: "updating multiple parameters on an idle instance IS allowed when the same plan is triggered",
+			old:  idle,
+			new: func() *Instance {
+				i := idle.DeepCopy()
+				i.Spec.Parameters = map[string]string{"foo": "newFoo", "other-foo": "newOtherFoo"}
+				return i
+			}(),
+			ov:   ov,
+			want: &deploy,
+		},
+		{
+			name: "parameter update on a scheduled instance IS allowed when the same plan is triggered",
+			old:  scheduled,
+			new: func() *Instance {
+				i := scheduled.DeepCopy()
+				i.Spec.Parameters = map[string]string{"foo": "newFoo"}
+				return i
+			}(),
+			ov:   ov,
+			want: nil,
+		},
+		{
+			name: "updating parameter on a scheduled instance IS NOT allowed when a different plan is triggered",
+			old:  scheduled,
+			new: func() *Instance {
+				i := scheduled.DeepCopy()
+				i.Spec.Parameters = map[string]string{"bar": "newBar"}
+				return i
+			}(),
+			ov:      ov,
+			wantErr: true,
+		},
+		{
+			name: "parameter update triggering multiple distinct plans IS NOT allowed",
+			old:  idle,
+			new: func() *Instance {
+				i := idle.DeepCopy()
+				i.Spec.Parameters = map[string]string{"foo": "newFoo", "bar": "newBar"}
+				return i
+			}(),
+			ov:      ov,
+			wantErr: true,
+		},
+		{
+			name: "parameter update triggering a non-existing ov plan IS allowed but will NOT trigger a plan",
+			old:  idle,
+			new: func() *Instance {
+				i := idle.DeepCopy()
+				i.Spec.Parameters["bazz"] = "newBazz"
+				return i
+			}(),
+			ov:   ov,
+			want: nil,
+		},
+		{
+			name: "parameter update together with an upgrade IS allowed",
+			old:  idle,
+			new: func() *Instance {
+				i := upgraded.DeepCopy()
+				i.Spec.Parameters = map[string]string{"foo": "newFoo", "bar": "newBar"}
+				return i
+			}(),
+			ov:   ov,
+			want: &update,
+		},
+		{
+			name: "parameter update together with a directly triggered plan IS allowed if the same plan is triggered",
+			old:  idle,
+			new: func() *Instance {
+				i := scheduled.DeepCopy()
+				i.Spec.Parameters = map[string]string{"foo": "newFoo"}
+				return i
+			}(),
+			ov:   ov,
+			want: &deploy,
+		},
+		{
+			name: "parameter update together with a directly triggered plan IS NOT allowed if different plans are triggered",
+			old:  idle,
+			new: func() *Instance {
+				i := scheduled.DeepCopy()
+				i.Spec.Parameters["bar"] = "newBar"
+				return i
+			}(),
+			ov:      ov,
+			wantErr: true,
+		},
 	}
 
 	for _, tt := range tests {
-		got, err := validateUpdate(&tt.old, &tt.new, tt.ov)
-		assert.Equal(t, tt.expectedError, err)
-		assert.Equal(t, tt.want, got)
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := validateUpdate(tt.old, tt.new, tt.ov)
+			assert.Equal(t, tt.wantErr, err != nil, "expected an error: %v but got: %v", tt.wantErr, err)
+			if err != nil {
+				log.Printf("err: %v", err)
+			}
+			assert.Equal(t, tt.want, got, "expected '%s' plan triggered but got: '%s'", stringPtrToString(tt.want), stringPtrToString(got))
+		})
 	}
+}
+
+func stringPtrToString(p *string) string {
+	if p != nil {
+		return *p
+	}
+	return "(nil)"
 }
 
 func Test_parameterDiffPlan(t *testing.T) {
@@ -109,6 +294,13 @@ func Test_parameterDiffPlan(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
+		{
+			name:    "no change doesn't trigger anything",
+			params:  []Parameter{},
+			ov:      ov,
+			want:    "",
+			wantErr: false,
+		},
 		{
 			name:    "param without an explicit trigger, triggers update plan",
 			params:  []Parameter{{Name: "foo"}},
