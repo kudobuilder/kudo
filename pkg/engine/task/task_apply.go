46535cf12de7b7cd6a6ea42dad7cac94e0478f4a
Andreas Neumann <aneumann@mesosphere.com>
2020-02-05 11:09:16 +0100

    Test for e2e with normal patch
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- pkg/engine/task/task_apply.go
+++ pkg/engine/task/task_apply.go
@@ -8,7 +8,6 @@ import (
 	v1 "k8s.io/api/core/v1"
 	apiextv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	apijson "k8s.io/apimachinery/pkg/util/json"
@@ -135,24 +134,111 @@ func patch(newObj runtime.Object, c client.Client) error {
 			return fmt.Errorf("failed to apply merge patch to object %s/%s: %w", key.Namespace, key.Name, err)
 		}
 	} else {
-		// For the strategic merge patch we want to add the "$path: replace" directive so that
-		// Lists get replaced and not merged.
-		us, err := runtime.DefaultUnstructuredConverter.ToUnstructured(newObj)
-		if err != nil {
-			return fmt.Errorf("failed to convert object to unstructured %s/%s: %w", key.Namespace, key.Name, err)
-		}
-		us["$patch"] = "replace"
-		usObj := unstructured.Unstructured{Object: us}
-
-		newObjJSON, _ := apijson.Marshal(usObj)
-		err = c.Patch(context.TODO(), &usObj, client.ConstantPatch(types.StrategicMergePatchType, newObjJSON))
-		if err != nil {
-			return fmt.Errorf("failed to apply StrategicMergePatch to object %s/%s: %w", key.Namespace, key.Name, err)
-		}
+		return patchNormalMerge(newObj, c)
+		//return patchStrategicMerge(newObj, c)
 	}
 
 	return nil
 }
+
+func patchNormalMerge(newObj runtime.Object, c client.Client) error {
+	newObjJSON, _ := apijson.Marshal(newObj)
+
+	// strategic merge patch is not supported for these types, falling back to merge patch
+	return c.Patch(context.TODO(), newObj, client.ConstantPatch(types.MergePatchType, newObjJSON))
+}
+
+//func patchStrategicMerge(newObj runtime.Object, c client.Client) error {
+//	key, _ := client.ObjectKeyFromObject(newObj)
+//
+//	// For the strategic merge patch we want to add the "$path: replace" directive so that
+//	// Lists get replaced and not merged.
+//	us, err := runtime.DefaultUnstructuredConverter.ToUnstructured(newObj)
+//	if err != nil {
+//		return fmt.Errorf("failed to convert object to unstructured %s/%s: %w", key.Namespace, key.Name, err)
+//	}
+//
+//	//us = markAllListsAsReplace(us).(map[string]interface{})
+//	//us = addPatchReplaceInRoot(us)
+//	us = addPatchReplaceToSelectedPaths(us)
+//
+//	newObjJSON, _ := apijson.Marshal(newObj)
+//	usObjJSON, _ := apijson.Marshal(us)
+//
+//	patchNew := make(map[string]interface{})
+//	patchUs := make(map[string]interface{})
+//
+//	err = apijson.Unmarshal(newObjJSON, &patchNew)
+//	if err != nil {
+//		fmt.Printf("Failed to unmarshal patch %v\n", err)
+//	}
+//	err = apijson.Unmarshal(usObjJSON, &patchUs)
+//	if err != nil {
+//		fmt.Printf("Failed to unmarshal patch %v\n", err)
+//	}
+//
+//	fmt.Printf("Sending patchNew %+v\n", patchNew)
+//	fmt.Printf("Sending patchUs  %+v\n", patchUs)
+//
+//	err = c.Patch(context.TODO(), newObj, client.ConstantPatch(types.StrategicMergePatchType, usObjJSON))
+//	if err != nil {
+//		return fmt.Errorf("failed to apply StrategicMergePatch to object %s/%s: %w", key.Namespace, key.Name, err)
+//	}
+//
+//	existing := &unstructured.Unstructured{}
+//	existing.GetObjectKind().SetGroupVersionKind(newObj.GetObjectKind().GroupVersionKind())
+//
+//	err = c.Get(context.TODO(), key, existing)
+//	if err != nil {
+//		fmt.Printf("Failed to get after patch %v\n", err)
+//	}
+//
+//	fmt.Printf("After Patching, the object is now: %+v\n", existing)
+//
+//	return nil
+//}
+
+//func addPatchReplaceToSelectedPaths(data map[string]interface{}) map[string]interface{} {
+//	paths := [][]string{
+//		{"spec", "template", "spec", "containers"},
+//		{"spec", "template", "spec", "initContainers"},
+//		{"spec", "template", "spec", "ephemeralContainers"},
+//	}
+//
+//	for _, path := range paths {
+//		list, found, _ := unstructured.NestedSlice(data, path...)
+//		if found {
+//			// TODO: This does not yet work if we remove the full list in newObj...
+//			list = append(list, map[string]interface{}{"$patch": "replace"})
+//			_ = unstructured.SetNestedSlice(data, list, path...)
+//		}
+//	}
+//
+//	return data
+//}
+
+//func addPatchReplaceInRoot(data map[string]interface{}) map[string]interface{} {
+//	data["$patch"] = "replace"
+//	return data
+//}
+
+//func markAllListsAsReplace(data interface{}) interface{} {
+//	if m, ok := data.(map[string]interface{}); ok {
+//		for k, v := range m {
+//			m[k] = markAllListsAsReplace(v)
+//		}
+//		return m
+//	}
+//	if l, ok := data.([]interface{}); ok {
+//		for i, v := range l {
+//			l[i] = markAllListsAsReplace(v)
+//		}
+//		l = append(l, map[string]string{"$patch": "replace"})
+//		return l
+//	}
+//
+//	return data
+//}
 
 func isKudoType(object runtime.Object) bool {
 	_, isOperator := object.(*v1beta1.OperatorVersion)
