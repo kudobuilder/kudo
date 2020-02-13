0d39b751ca5ea34cb15497e2bf73791740fb3171
Andreas Neumann <aneumann@mesosphere.com>
2020-02-13 12:58:16 +0100

    Adjusted use Result by new Verifiers
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- pkg/kudoctl/packages/verifier/task/verify_build.go
+++ pkg/kudoctl/packages/verifier/task/verify_build.go
@@ -2,22 +2,24 @@ package task
 
 import (
 	"github.com/kudobuilder/kudo/pkg/engine/task"
-
 	"github.com/kudobuilder/kudo/pkg/kudoctl/packages"
 	"github.com/kudobuilder/kudo/pkg/kudoctl/packages/verifier"
+	"github.com/kudobuilder/kudo/pkg/kudoctl/verify"
 )
+
+var _ verifier.PackageVerifier = &BuildVerifier{}
 
 // BuildVerifier verifies tasks producing errors for tasks referenced in plans that do not exist and warnings for tasks which are not used in a plan
 type BuildVerifier struct{}
 
-func (BuildVerifier) Verify(pf *packages.Files) verifier.Result {
-	res := verifier.NewResult()
+func (BuildVerifier) Verify(pf *packages.Files) verify.Result {
+	res := verify.NewResult()
 	res.Merge(tasksWellDefined(pf))
 	return res
 }
 
-func tasksWellDefined(pf *packages.Files) verifier.Result {
-	result := verifier.NewResult()
+func tasksWellDefined(pf *packages.Files) verify.Result {
+	result := verify.NewResult()
 	for _, tt := range pf.Operator.Tasks {
 		tt := tt
 
--- pkg/kudoctl/packages/verifier/template/verify_render.go
+++ pkg/kudoctl/packages/verifier/template/verify_render.go
@@ -4,6 +4,7 @@ import (
 	"github.com/kudobuilder/kudo/pkg/engine/renderer"
 	"github.com/kudobuilder/kudo/pkg/kudoctl/packages"
 	"github.com/kudobuilder/kudo/pkg/kudoctl/packages/verifier"
+	"github.com/kudobuilder/kudo/pkg/kudoctl/verify"
 )
 
 var _ verifier.PackageVerifier = &RenderVerifier{}
@@ -11,13 +12,13 @@ var _ verifier.PackageVerifier = &RenderVerifier{}
 // RenderVerifier checks that all templates are compilable and contain valid golang template syntax
 type RenderVerifier struct{}
 
-func (RenderVerifier) Verify(pf *packages.Files) verifier.Result {
-	res := verifier.NewResult()
+func (RenderVerifier) Verify(pf *packages.Files) verify.Result {
+	res := verify.NewResult()
 	res.Merge(templateCompilable(pf))
 	return res
 }
 
-func templateCompilable(pf *packages.Files) verifier.Result {
+func templateCompilable(pf *packages.Files) verify.Result {
 	params := make(map[string]string)
 	for _, p := range pf.Params.Parameters {
 		params[p.Name] = "default"
@@ -34,7 +35,7 @@ func templateCompilable(pf *packages.Files) verifier.Result {
 	configs["StepName"] = "StepName"
 	configs["AppVersion"] = "AppVersion"
 
-	res := verifier.NewResult()
+	res := verify.NewResult()
 
 	engine := renderer.New()
 	for k, v := range pf.Templates {
