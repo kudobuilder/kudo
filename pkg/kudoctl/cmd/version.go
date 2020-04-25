deaf3e4710255fef0c7264a189caf3bdc3da2342
Sivaram Kannan <sivaramsk@gmail.com>
2020-04-25 16:47:22 +0530

    Add function to print controller version and print it.

--- pkg/kudoctl/cmd/version.go
+++ pkg/kudoctl/cmd/version.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/kudobuilder/kudo/pkg/kudoctl/clog"
+	"github.com/kudobuilder/kudo/pkg/kudoctl/kube"
 	"github.com/kudobuilder/kudo/pkg/version"
 )
 
@@ -30,5 +33,33 @@ func newVersionCmd() *cobra.Command {
 func VersionCmd(cmd *cobra.Command, args []string) error {
 	kudoVersion := version.Get()
 	fmt.Printf("KUDO Version: %s\n", fmt.Sprintf("%#v", kudoVersion))
+
+	// Print the Controller Version
+	fmt.Printf("KUDO Controller Version: %s\n", GetControllerVersion())
+
 	return nil
+}
+
+// GetControllerVersion
+func GetControllerVersion() string {
+
+	controllerVersion := "<Controller Not Running>"
+
+	client, err := kube.GetKubeClient(Settings.KubeConfig)
+	clog.V(3).Printf("acquiring kudo client")
+	if err != nil {
+		clog.V(3).Printf("failed to acquire client")
+		return controllerVersion
+	}
+
+	statefulsets, err := client.KubeClient.AppsV1().StatefulSets("").List(metav1.ListOptions{LabelSelector: "app=kudo-manager"})
+	if err != nil {
+		return controllerVersion
+	}
+
+	for _, d := range statefulsets.Items {
+		controllerVersion = d.Spec.Template.Spec.Containers[0].Image
+	}
+
+	return controllerVersion
 }
