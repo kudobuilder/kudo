78879d12d9c3f6e59bce6a808a833494946b3ae0
Ken Sipe <kensipe@gmail.com>
2020-05-27 09:17:35 -0500

    adding documentation
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

--- hack/run-operator-tests.sh
+++ hack/run-operator-tests.sh
@@ -5,7 +5,13 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
+# Operator tests have the same setup but different concerns from E2E tests. 
+# The motivation is to get earlier feedback when these tests fail. In the past, the operator tests were only run if the E2E tests succeeded. 
+# This made it hard for larger PRs to distinguish between changes that fix operators and changes that fix E2E tests.
+
 INTEGRATION_OUTPUT_JUNIT=${INTEGRATION_OUTPUT_JUNIT:-false}
+# Exposing `KUDO_VERSION` allows for KUDO_VERSION to be overriden. The default value of test is used as a tag for the Docker image that is build and injected to the kind cluster. 
+# This is to avoid clashes with existing Docker images as test is only used in this context.
 KUDO_VERSION=${KUDO_VERSION:-test}
 
 docker build . \
