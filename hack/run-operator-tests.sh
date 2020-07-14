2bb200f15f7426bc014f59f8141cc000b22aa4d3
Andreas Neumann <aneumann@mesosphere.com>
2020-07-14 13:59:20 +0200

    Store kind logs in correct path for operator tests
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- hack/run-operator-tests.sh
+++ hack/run-operator-tests.sh
@@ -23,7 +23,6 @@ git clone https://github.com/kudobuilder/operators
 mkdir operators/bin/
 cp ./bin/kubectl-kudo operators/bin/
 sed "s/%version%/$KUDO_VERSION/" operators/kudo-test.yaml.tmpl > operators/kudo-test.yaml
-echo 'artifactsDir: ../reports/kind-logs' >> operators/kudo-test.yaml
 
 if [ "$INTEGRATION_OUTPUT_JUNIT" == true ]
 then
@@ -31,12 +30,12 @@ then
     mkdir -p reports/
     go get github.com/jstemmer/go-junit-report
 
-    cd operators && ./bin/kubectl-kudo test 2>&1 \
+    cd operators && ./bin/kubectl-kudo test --artifacts-dir ../reports/kind-logs 2>&1 \
         | tee /dev/fd/2 \
         | go-junit-report -set-exit-code \
         > ../reports/kudo_operators_test_report.xml
 else
     echo "Running operator tests without junit output"
 
-    cd operators && ./bin/kubectl-kudo test
+    cd operators && ./bin/kubectl-kudo test --artifacts-dir ../reports/kind-logs
 fi
