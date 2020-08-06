69865fbdd8cd5c2637991562c81b00f824722c14
Andreas Neumann <aneumann@mesosphere.com>
2020-08-06 19:03:16 +0200

    Removed some more code duplication, return error if invalid output type is given
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- pkg/kudoctl/cmd/output/output.go
+++ pkg/kudoctl/cmd/output/output.go
@@ -15,58 +15,52 @@ const (
 )
 
 func WriteObjects(objs []interface{}, outputType string, out io.Writer) error {
-	var o []byte
-	var err error
 	if strings.ToLower(outputType) == TypeYAML {
+		// Write YAML objects with separators
 		for _, obj := range objs {
-			o, err = yaml.Marshal(obj)
-			if err != nil {
-				return fmt.Errorf("failed to marshal to yaml: %v", err)
-			}
 			if _, err := fmt.Fprintln(out, "---"); err != nil {
 				return err
 			}
 
-			if _, err := fmt.Fprintln(out, string(o)); err != nil {
+			if err := writeObjectYAML(obj, out); err != nil {
 				return err
 			}
 		}
 
 		// YAML ending document boundary marker
-		_, err = fmt.Fprintln(out, "...")
-
+		_, err := fmt.Fprintln(out, "...")
 		return err
 	} else if strings.ToLower(outputType) == TypeJSON {
-		o, err = json.MarshalIndent(objs, "", "  ")
-		if err != nil {
-			return fmt.Errorf("failed to marshal to json: %v", err)
-		}
-		if _, err := fmt.Fprintln(out, string(o)); err != nil {
-			return err
-		}
-		return nil
+		return writeObjectJSON(objs, out)
 	}
 
-	return nil
+	return fmt.Errorf("invalid output format, only support yaml or json")
 }
 
 func WriteObject(obj interface{}, outputType string, out io.Writer) error {
-	var o []byte
-	var err error
 	if strings.ToLower(outputType) == TypeYAML {
-		o, err = yaml.Marshal(obj)
-		if err != nil {
-			return fmt.Errorf("failed to marshal to yaml: %v", err)
-		}
+		return writeObjectYAML(obj, out)
 	} else if strings.ToLower(outputType) == TypeJSON {
-		o, err = json.MarshalIndent(obj, "", "  ")
-		if err != nil {
-			return fmt.Errorf("failed to marshal to json: %v", err)
-		}
+		return writeObjectJSON(obj, out)
 	}
 
-	if _, err := fmt.Fprintln(out, string(o)); err != nil {
-		return err
+	return fmt.Errorf("invalid output format, only support yaml or json")
+}
+
+func writeObjectJSON(obj interface{}, out io.Writer) error {
+	o, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal to json: %v", err)
 	}
-	return nil
+	_, err = fmt.Fprintln(out, string(o))
+	return err
+}
+
+func writeObjectYAML(obj interface{}, out io.Writer) error {
+	o, err := yaml.Marshal(obj)
+	if err != nil {
+		return fmt.Errorf("failed to marshal to yaml: %v", err)
+	}
+	_, err = fmt.Fprintln(out, string(o))
+	return err
 }
