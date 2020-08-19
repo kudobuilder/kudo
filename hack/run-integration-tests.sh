fa5fda111c5118387ca838f9118040de1a9cc7b6
Andreas Neumann <aneumann@mesosphere.com>
2020-08-19 14:31:14 +0200

    Separate execution of Go integration tests and kuttl integration tests in Makefile so running a single suite is easier
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- hack/run-integration-tests.sh
+++ hack/run-integration-tests.sh
@@ -6,17 +6,22 @@ set -o pipefail
 set -o xtrace
 
 INTEGRATION_OUTPUT_JUNIT=${INTEGRATION_OUTPUT_JUNIT:-false}
-TEST_ONLY=${TEST_ONLY:+"--test $TEST_ONLY"}
+
+MOD_FLAGS="-mod=readonly"
+
+# When run from a Goland/IntelliJ terminal, Goland/IntelliJ already set '-mod=readonly'
+if [ "${_INTELLIJ_FORCE_SET_GOFLAGS+x}" ]
+then
+   MOD_FLAGS=""
+fi
 
 if [ "$INTEGRATION_OUTPUT_JUNIT" == true ]
 then
     echo "Running integration tests with junit output"
     mkdir -p reports/
     go get github.com/jstemmer/go-junit-report
-    go test -tags integration ./pkg/... ./cmd/... -v -mod=readonly -coverprofile cover-integration.out 2>&1 |tee /dev/fd/2 |go-junit-report -set-exit-code > reports/integration_report.xml
-    go run ./cmd/kubectl-kudo test --config test/kudo-integration-test.yaml ${TEST_ONLY} 2>&1 |tee /dev/fd/2 |go-junit-report -set-exit-code > reports/kudo_test_report.xml
+    go test -tags integration ./pkg/... ./cmd/... -v ${MOD_FLAGS} -coverprofile cover-integration.out 2>&1 |tee /dev/fd/2 |go-junit-report -set-exit-code > reports/integration_report.xml
 else
     echo "Running integration tests without junit output"
-    go test -tags integration ./pkg/... ./cmd/... -v -mod=readonly -coverprofile cover-integration.out
-    go run ./cmd/kubectl-kudo test --config test/kudo-integration-test.yaml ${TEST_ONLY}
+    go test -tags integration ./pkg/... ./cmd/... -v ${MOD_FLAGS} -coverprofile cover-integration.out
 fi
