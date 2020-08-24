ccf525cfc0bb24d4e42f9a7c30da6b730ae8b557
Andreas Neumann <aneumann@mesosphere.com>
2020-08-24 14:12:40 +0200

    Renamed old-kudo binary file to make intention more clear
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- test/upgrade/upgrade-to-current/download-kudo.sh
+++ test/upgrade/upgrade-to-current/download-kudo.sh
@@ -6,9 +6,9 @@ echo "Downloading KUDO v${PREV_KUDO_VERSION}"
 
 # Download previous KUDO version for upgrade testing
 if [[ "$(uname)" == "Darwin" ]]; then
-    curl -L https://github.com/kudobuilder/kudo/releases/download/v${PREV_KUDO_VERSION}/kubectl-kudo_${PREV_KUDO_VERSION}_darwin_x86_64 --output kubectl-kudo
+    curl -L https://github.com/kudobuilder/kudo/releases/download/v${PREV_KUDO_VERSION}/kubectl-kudo_${PREV_KUDO_VERSION}_darwin_x86_64 --output old-kudo
 elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
-    curl -L https://github.com/kudobuilder/kudo/releases/download/v${PREV_KUDO_VERSION}/kubectl-kudo_${PREV_KUDO_VERSION}_linux_x86_64 --output kubectl-kudo
+    curl -L https://github.com/kudobuilder/kudo/releases/download/v${PREV_KUDO_VERSION}/kubectl-kudo_${PREV_KUDO_VERSION}_linux_x86_64 --output old-kudo
 fi
 
-chmod +x kubectl-kudo
+chmod +x old-kudo
