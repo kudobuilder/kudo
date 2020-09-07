505c5269e0cd44717a0ea46a667b41971b4ef3bc
Jan Schlicht <jan@d2iq.com>
2020-09-07 10:37:38 +0200

    Use short package names
    Signed-off-by: Jan Schlicht <jan@d2iq.com>

--- pkg/engine/health/health.go
+++ pkg/engine/health/health.go
@@ -10,8 +10,8 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apiextv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -63,18 +63,18 @@ func IsHealthy(obj runtime.Object) error {
 
 	objUnstructured := &unstructured.Unstructured{Object: unstructMap}
 	switch obj := obj.(type) {
-	case *apiextensionsv1beta1.CustomResourceDefinition:
+	case *apiextv1beta1.CustomResourceDefinition:
 		for _, c := range obj.Status.Conditions {
-			if c.Type == apiextensionsv1beta1.Established && c.Status == apiextensionsv1beta1.ConditionTrue {
+			if c.Type == apiextv1beta1.Established && c.Status == apiextv1beta1.ConditionTrue {
 				log.Printf("CRD %s is now healthy", obj.Name)
 				return nil
 			}
 		}
 		msg := fmt.Sprintf("CRD %s is not healthy ( Conditions: %v )", obj.Name, obj.Status.Conditions)
 		return errors.New(msg)
-	case *apiextensionsv1.CustomResourceDefinition:
+	case *apiextv1.CustomResourceDefinition:
 		for _, c := range obj.Status.Conditions {
-			if c.Type == apiextensionsv1.Established && c.Status == apiextensionsv1.ConditionTrue {
+			if c.Type == apiextv1.Established && c.Status == apiextv1.ConditionTrue {
 				log.Printf("CRD %s is now healthy", obj.Name)
 				return nil
 			}
