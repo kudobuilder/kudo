d54c55643993122ae8407bcff9ce162cf5b6861e
Andreas Neumann <aneumann@mesosphere.com>
2020-09-16 14:16:13 +0200

    Cleanup migrations
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- pkg/kudoctl/kudoinit/migration/01_migrate_ov_names/migrate_ov_names.go
+++ pkg/kudoctl/kudoinit/migration/01_migrate_ov_names/migrate_ov_names.go
@@ -5,9 +5,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/thoas/go-funk"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	kudoapi "github.com/kudobuilder/kudo/pkg/apis/kudo/v1beta1"
 	"github.com/kudobuilder/kudo/pkg/kudoctl/clog"
@@ -22,6 +20,7 @@ var (
 type MigrateOvNames struct {
 	client *kube.Client
 	dryRun bool
+	ctx    context.Context
 
 	migratedOVs map[string]*kudoapi.OperatorVersion
 }
@@ -30,6 +29,7 @@ func New(client *kube.Client, dryRun bool) *MigrateOvNames {
 	return &MigrateOvNames{
 		client:      client,
 		dryRun:      dryRun,
+		ctx:         context.TODO(),
 		migratedOVs: map[string]*kudoapi.OperatorVersion{},
 	}
 }
@@ -40,6 +40,9 @@ func (m *MigrateOvNames) CanMigrate() error {
 }
 
 func (m *MigrateOvNames) Migrate() error {
+	if !m.dryRun {
+		clog.V(0).Printf("Migrate OperatorVersion names")
+	}
 	return migration.ForEachNamespace(m.client, m.migrateInNamespace)
 }
 
@@ -77,18 +80,18 @@ func (m *MigrateOvNames) migrateOperatorVersion(ov *kudoapi.OperatorVersion) err
 	if !m.dryRun {
 		var err error
 		clog.V(4).Printf("Create copy of OperatorVersion with name %q", ov.Name)
-		ov, err = m.client.KudoClient.KudoV1beta1().OperatorVersions(ov.Namespace).Create(context.TODO(), ov, v1.CreateOptions{})
+		ov, err = m.client.KudoClient.KudoV1beta1().OperatorVersions(ov.Namespace).Create(m.ctx, ov, v1.CreateOptions{})
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to create copy of operator version: %v", err)
 		}
 	}
 
-	// Store migrated operator versions for instance owner references
+	// Store migrated operator versions for instance migration
 	m.migratedOVs[oldName] = ov
 
 	if !m.dryRun {
 		clog.V(4).Printf("Delete old OperatorVersion with name %q", oldName)
-		if err := m.client.KudoClient.KudoV1beta1().OperatorVersions(ov.Namespace).Delete(context.TODO(), oldName, v1.DeleteOptions{}); err != nil {
+		if err := m.client.KudoClient.KudoV1beta1().OperatorVersions(ov.Namespace).Delete(m.ctx, oldName, v1.DeleteOptions{}); err != nil {
 			return fmt.Errorf("failed to delete old OperatorVersion %q: %v", oldName, err)
 		}
 	}
@@ -98,25 +101,17 @@ func (m *MigrateOvNames) migrateOperatorVersion(ov *kudoapi.OperatorVersion) err
 func (m *MigrateOvNames) migrateInstance(i *kudoapi.Instance) error {
 	newOwner, ovWasMigrated := m.migratedOVs[i.Spec.OperatorVersion.Name]
 	if ovWasMigrated {
-		oldName := i.Spec.OperatorVersion.Name
-		clog.V(1).Printf("Adjust OperatorVersion and owner reference of Instance %q", i.Name)
+		clog.V(1).Printf("Adjust OperatorVersion of Instance %q", i.Name)
 
 		// Set OperatorVersion
 		i.Spec.OperatorVersion.Name = newOwner.Name
 
-		// Replace OwnerReference
-		//nolint:errcheck
-		i.OwnerReferences = funk.Filter(i.OwnerReferences, func(o v1.OwnerReference) bool { return o.Name != oldName }).([]v1.OwnerReference)
-		if err := controllerutil.SetOwnerReference(newOwner, i, m.client.Scheme); err != nil {
-			return fmt.Errorf("failed to set resource ownership for the new instance: %v", err)
-		}
-
 		// Save update
 		if !m.dryRun {
 			clog.V(4).Printf("Update instance %q with new owner reference", i.Name)
-			_, err := m.client.KudoClient.KudoV1beta1().Instances(i.Namespace).Update(context.TODO(), i, v1.UpdateOptions{})
+			_, err := m.client.KudoClient.KudoV1beta1().Instances(i.Namespace).Update(m.ctx, i, v1.UpdateOptions{})
 			if err != nil {
-				return fmt.Errorf("failed to update Instance %q with new owner reference: %v", i.Name, err)
+				return fmt.Errorf("failed to update Instance %q with new operator version: %v", i.Name, err)
 			}
 		}
 	}
