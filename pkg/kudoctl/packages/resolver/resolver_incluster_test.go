e0cade5768fe7ee88531bd8b14ab4c990b977a90
Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>
2020-09-17 12:59:21 +0200

    Using the right assert
    Signed-off-by: Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>

--- pkg/kudoctl/packages/resolver/resolver_incluster_test.go
+++ pkg/kudoctl/packages/resolver/resolver_incluster_test.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"testing"
 
-	"gotest.tools/assert"
+	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubefake "k8s.io/client-go/kubernetes/fake"
