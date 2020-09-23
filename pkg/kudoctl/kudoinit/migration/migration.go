108c854f521b2be6ea4bbaa607a9437bb0293ba3
Andreas Neumann <aneumann@mesosphere.com>
2020-09-23 14:18:08 +0200

    Updated documentation
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- pkg/kudoctl/kudoinit/migration/migration.go
+++ pkg/kudoctl/kudoinit/migration/migration.go
@@ -1,9 +1,13 @@
 package migration
 
+// Migrater is the base interface for migrations.
+
 type Migrater interface {
 	// CanMigrate checks if there are any conditions that would prevent this migration to run
+	// This function should only return an error if it is sure that the migration can not be executed
 	CanMigrate() error
 
-	// Migrate executes the
+	// Migrate executes the migration. The call must be idempotent and ignore already migrated resources
+	// It can be called multiple times on the same cluster and encounter migrated and unmigrated resources.
 	Migrate() error
 }
