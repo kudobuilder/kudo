bb2d47e5ce885685deb5c5a7119dcb52510bf745
Ken Sipe <kensipe@gmail.com>
2022-02-17 07:06:47 -0600

    Creating new Go 1.17 build image
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

--- build/test-images.sh
+++ build/test-images.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker build --build-arg GOLANG_VERSION=1.15 -t kudobuilder/golang:1.15 .
-docker push kudobuilder/golang:1.15
+docker build --build-arg GOLANG_VERSION=1.17 -t kudobuilder/golang:1.17 .
+docker push kudobuilder/golang:1.17
 
 docker build --build-arg GOLANG_VERSION=latest -t kudobuilder/golang:latest .
 docker push kudobuilder/golang:latest
