340e771a847daff9ce77305a33ff30ac48b81832
Ken Sipe <kensipe@gmail.com>
2022-03-16 16:05:02 -0500

    Version Bump for Go Build Docker Image
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

--- build/test-images.sh
+++ build/test-images.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker build --build-arg GOLANG_VERSION=1.15 -t kudobuilder/golang:1.15 .
-docker push kudobuilder/golang:1.15
+docker build --build-arg GOLANG_VERSION=1.18 -t kudobuilder/golang:1.18 .
+docker push kudobuilder/golang:1.18
 
 docker build --build-arg GOLANG_VERSION=latest -t kudobuilder/golang:latest .
 docker push kudobuilder/golang:latest
